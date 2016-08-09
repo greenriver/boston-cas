fa326775e185e78500426fe4217b76972f62c3ef
Elliot Anders <eanders@greenriver.org>
2016-08-09 16:04:57 -0400

    Updating total voucher count to match currently created vouchers

--- db/migrate/20160809193948_add_voucher_count_to_sub_program.rb
+++ db/migrate/20160809193948_add_voucher_count_to_sub_program.rb
@@ -1,5 +1,16 @@
 class AddVoucherCountToSubProgram < ActiveRecord::Migration
-  def change
+  def up
     add_column :sub_programs, :voucher_count, :integer, default: 0
+    sp = SubProgram.all
+    sp.each do |s|
+      if s.vouchers.count > 0
+        s.update_attribute(:voucher_count, s.vouchers.count)
+      end
+    end
+
+  end
+
+  def down
+    remove_column :sub_programs, :voucher_count
   end
 end
