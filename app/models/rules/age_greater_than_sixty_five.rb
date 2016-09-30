2a115b0739f7a9cb8a08cef2f2a4e97d9cc95d30
Elliot Anders <eanders@greenriver.org>
2016-09-30 09:06:22 -0400

    PT [#130774943] Adding rules for 45 and 65

---
+++ app/models/rules/age_greater_than_sixty_five.rb
@@ -0,0 +1,15 @@
+class Rules::AgeGreaterThanSixtyFive < Rule
+  def clients_that_fit(scope, requirement)
+    if date_of_birth = Client.arel_table[:date_of_birth]
+      years_ago = Date.today - 65.years
+      if requirement.positive
+        where = "date_of_birth < ?"
+      else
+        where = "date_of_birth >= ?"
+      end
+      scope.where(where, years_ago)
+    else
+      raise RuleDatabaseStructureMissing.new("clients.hiv_aids missing. Cannot check clients against #{self.class}.")
+    end
+  end
+end
