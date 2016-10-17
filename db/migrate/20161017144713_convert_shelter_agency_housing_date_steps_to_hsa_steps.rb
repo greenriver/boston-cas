25b9ed6fe2774dda31f03a9c72afde457ab8209b
Elliot Anders <eanders@greenriver.org>
2016-10-17 11:19:06 -0400

    Making migration reversable

--- db/migrate/20161017144713_convert_shelter_agency_housing_date_steps_to_hsa_steps.rb
+++ db/migrate/20161017144713_convert_shelter_agency_housing_date_steps_to_hsa_steps.rb
@@ -1,5 +1,5 @@
 class ConvertShelterAgencyHousingDateStepsToHsaSteps < ActiveRecord::Migration
-  def change
+  def up
     shelter_agency_pending_decisions = MatchDecisions::Base.where(type: 'MatchDecisions::RecordClientHousedDateShelterAgency', status: 'pending')
     shelter_agency_pending_decisions.each do |d|
       d.update(type: 'MatchDecisions::RecordClientHousedDateHousingSubsidyAdministrator')
@@ -7,6 +7,17 @@ class ConvertShelterAgencyHousingDateStepsToHsaSteps < ActiveRecord::Migration
     shelter_agency_future_decisions = MatchDecisions::Base.where(type: 'MatchDecisions::RecordClientHousedDateShelterAgency', status: nil)
     shelter_agency_future_decisions.each do |d|
       d.update(type: 'MatchDecisions::RecordClientHousedDateHousingSubsidyAdministrator')
+    end
+  end
+
+  def down
+    hsa_pending_decisions = MatchDecisions::Base.where(type: 'MatchDecisions::RecordClientHousedDateHousingSubsidyAdministrator', status: 'pending')
+    hsa_pending_decisions.each do |d|
+      d.update(type: 'MatchDecisions::RecordClientHousedDateShelterAgency')
+    end
+    hsa_future_decisions = MatchDecisions::Base.where(type: 'MatchDecisions::RecordClientHousedDateHousingSubsidyAdministrator', status: nil)
+    hsa_future_decisions.each do |d|
+      d.update(type: 'MatchDecisions::RecordClientHousedDateShelterAgency')
     end
   end
 end
