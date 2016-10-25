c114bcd0dcb7613e97b9e179c6a3a9f7c0dd7885
Elliot Anders <eanders@greenriver.org>
2016-10-25 13:38:06 -0400

    Moved fancy sorting out of match list base onto active matches only

--- app/controllers/active_matches_controller.rb
+++ app/controllers/active_matches_controller.rb
@@ -1,5 +1,39 @@
 class ActiveMatchesController < MatchListBaseController
 
+  def index
+    # search
+    @matches = if params[:q].present?
+      match_scope.text_search(params[:q])
+    else
+      match_scope
+    end
+    # decision subquery
+
+    md = MatchDecisions::Base.where(
+      'match_id = client_opportunity_matches.id'
+    ).where.not(status: nil).order(created_at: :desc).limit(1)
+
+    # sort / paginate
+    column = "client_opportunity_matches.#{sort_column}"
+    if sort_column == 'calculated_first_homeless_night'
+      column = 'clients.calculated_first_homeless_night'
+    elsif sort_column == 'client_id'
+      column = 'clients.last_name'
+    elsif sort_column == 'last_decision'
+      column = "last_decision.updated_at"
+    elsif sort_column == 'current_step'
+      column = 'last_decision.type'
+    end
+    sort = "#{column} #{sort_direction}"
+    @matches = @matches
+      .references(:client)
+      .includes(:client)
+      .joins("CROSS JOIN LATERAL (#{md.to_sql}) last_decision")
+      .order(sort)
+      .preload(:client, :opportunity, :decisions)
+      .page(params[:page]).per(25)
+  end
+  
   private
   
     def match_scope
--- app/controllers/match_list_base_controller.rb
+++ app/controllers/match_list_base_controller.rb
@@ -11,11 +11,6 @@ class MatchListBaseController < ApplicationController
     else
       match_scope
     end
-    # decision subquery
-
-    md = MatchDecisions::Base.where(
-      'match_id = client_opportunity_matches.id'
-    ).where.not(status: nil).order(created_at: :desc).limit(1)
 
     # sort / paginate
     column = "client_opportunity_matches.#{sort_column}"
@@ -23,17 +18,11 @@ class MatchListBaseController < ApplicationController
       column = 'clients.calculated_first_homeless_night'
     elsif sort_column == 'client_id'
       column = 'clients.last_name'
-    elsif sort_column == 'last_decision'
-      column = "last_decision.updated_at"
-    elsif sort_column == 'current_step'
-      column = 'last_decision.type'
     end
     sort = "#{column} #{sort_direction}"
-
     @matches = @matches
       .references(:client)
       .includes(:client)
-      .joins("CROSS JOIN LATERAL (#{md.to_sql}) last_decision")
       .order(sort)
       .preload(:client, :opportunity, :decisions)
       .page(params[:page]).per(25)
