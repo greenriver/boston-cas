1b19beb990e829b883601fcc91811d6b33565a6b
Elliot Anders <eanders@greenriver.org>
2017-02-28 16:22:28 -0500

    Allowing parking from all locations where you can cancel

--- app/models/match_decisions/base.rb
+++ app/models/match_decisions/base.rb
@@ -164,7 +164,7 @@ module MatchDecisions
     end
     
     def permitted_params
-      [:status, :note]
+      [:status, :note, :prevent_matching_until]
     end
 
     def whitelist_params_for_update params
