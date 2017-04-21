aee7a6d7a28e719c9810b569af6a85ff525829ee
Mark McCraw <mmccraw@greenriver.org>
2017-04-21 10:20:49 -0400

    updating migrations to deal with paranoia scopes, making migrations more tolerant of column changes, updating CSV data to reflect current schema for test data imports

--- db/migrate/20161117155843_add_dmh_eligible_permission.rb
+++ db/migrate/20161117155843_add_dmh_eligible_permission.rb
@@ -1,9 +1,15 @@
 class AddDmhEligiblePermission < ActiveRecord::Migration
-  def change
-    add_column :roles, :can_view_dmh_eligibility, :boolean, null: false, default: false
-    admin = Role.where(name: 'admin').first
-    dnd = Role.where(name: 'dnd_staff').first
-    admin.update(can_view_dmh_eligibility: true)
-    dnd.update(can_view_dmh_eligibility: true)
+  def up
+    unless column_exists?(:roles, :can_view_dmh_eligibility)
+      add_column :roles, :can_view_dmh_eligibility, :boolean, null: false, default: false
+      admin = Role.where(name: 'admin').first
+      dnd = Role.where(name: 'dnd_staff').first
+      admin.update(can_view_dmh_eligibility: true)
+      dnd.update(can_view_dmh_eligibility: true)
+    end
+  end
+
+  def down
+    remove_column :roles, :can_view_dmh_eligibility, :boolean, null: false, default: false
   end
 end
--- db/migrate/20161130173742_add_va_and_hues_eligibility.rb
+++ db/migrate/20161130173742_add_va_and_hues_eligibility.rb
@@ -1,14 +1,25 @@
 class AddVaAndHuesEligibility < ActiveRecord::Migration
-  def change
+  def up
     add_column :project_clients, :va_eligible, :boolean, null: false, default: false
     add_column :clients, :va_eligible, :boolean, null: false, default: false
     add_column :project_clients, :hues_eligible, :boolean, null: false, default: false
     add_column :clients, :hues_eligible, :boolean, null: false, default: false
-    add_column :roles, :can_view_va_eligibility, :boolean, null: false, default: false
-    add_column :roles, :can_view_hues_eligibility, :boolean, null: false, default: false
+    unless column_exists? :roles, :can_view_va_eligibility
+        add_column :roles, :can_view_va_eligibility, :boolean, null: false, default: false
+        add_column :roles, :can_view_hues_eligibility, :boolean, null: false, default: false
+    end
     admin = Role.where(name: 'admin').first
     dnd = Role.where(name: 'dnd_staff').first
     admin.update({can_view_va_eligibility: true, can_view_hues_eligibility: true})
     dnd.update({can_view_va_eligibility: true, can_view_hues_eligibility: true})
+  end
+
+  def down
+    remove_column :project_clients, :va_eligible, :boolean, null: false, default: false
+    remove_column :clients, :va_eligible, :boolean, null: false, default: false
+    remove_column :project_clients, :hues_eligible, :boolean, null: false, default: false
+    remove_column :clients, :hues_eligible, :boolean, null: false, default: false
+    remove_column :roles, :can_view_va_eligibility, :boolean, null: false, default: false
+    remove_column :roles, :can_view_hues_eligibility, :boolean, null: false, default: false
   end
 end
