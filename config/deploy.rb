0cb0a437df4b2d101944c370f8931e86dcca364f
Elliot Anders <eanders@greenriver.org>
2017-07-21 10:57:15 -0400

    Using ENV for database and exception notifier

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,10 @@ end
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/exception_notifier.yml', '.env')
+set :linked_files, fetch(:linked_files, []).push(
+  'config/secrets.yml',
+  '.env'
+)
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'var')
