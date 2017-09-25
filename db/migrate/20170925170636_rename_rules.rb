5877ec8b029bcf434ebce0f7cbe665f6052b85ca
Elliot Anders <eanders@greenriver.org>
2017-09-25 13:23:38 -0400

    Adjusting rule rename migration

--- db/migrate/20170925170636_rename_rules.rb
+++ db/migrate/20170925170636_rename_rules.rb
@@ -1,6 +1,8 @@
 class RenameRules < ActiveRecord::Migration
-  def change
-    Rules::OneEightyDaysHomeless.first.update(name: '180 homeless days')
-    Rules::OneYearHomeless.first.update(name: '365 homeless days')
+  def up
+    rule = Rules::OneEightyDaysHomeless&.first
+    rule.update(name: '180 homeless days') if rule.present?
+    rule = Rules::OneYearHomeless&.first
+    rule.update(name: '365 homeless days') if rule.present?
   end
 end
