47ddc8fd42055e68fc5665021d9b0ac2306996f8
Elliot Anders <eanders@greenriver.org>
2017-10-02 14:52:39 -0400

    Adding defaults to boolean client columns

--- db/migrate/20171002184557_add_defaults_to_boolean_columns_in_clients.rb
+++ db/migrate/20171002184557_add_defaults_to_boolean_columns_in_clients.rb
@@ -1,6 +1,8 @@
 class AddDefaultsToBooleanColumnsInClients < ActiveRecord::Migration
   def change
     change_column :clients, :veteran, :boolean, default: false
+    Client.where(veteran: nil).update_all(veteran: false)
     change_column :clients, :chronic_homeless, :boolean, default: false
+    Client.where(chronic_homeless: nil).update_all(chronic_homeless: false)
   end
 end
