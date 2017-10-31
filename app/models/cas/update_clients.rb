1294c4270fc04aa906822f99a076d6e6d746bf37
Peter Clark <peter@greenriver.org>
2017-10-31 11:49:11 -0500

    add last_seen to update_clients task

--- app/models/cas/update_clients.rb
+++ app/models/cas/update_clients.rb
@@ -143,7 +143,8 @@ module Cas
         :child_in_household,
         :days_homeless,
         :days_homeless_in_last_three_years,
-        :ha_eligible
+        :ha_eligible,
+        :last_seen
       ).first
       pc_attr = pc.attributes.map do |k,v|
         [k.to_sym, v]
