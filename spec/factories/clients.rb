615312b7699409c4942a8c4e367fc4d23e80694a
Elliot Anders <eanders@greenriver.org>
2017-11-10 11:10:42 -0500

    Match engine tests

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -7,5 +7,8 @@ FactoryGirl.define do
     available_candidate true
     gender_id 0 # female
     date_of_birth '1990-01-01'
+    vispdat_score Faker::Number.between(1, 20)
+    vispdat_priority_score Faker::Number.between(1, 800)
+    calculated_first_homeless_night Faker::Date.between(20.years.ago, 1.years.ago)
   end
 end
--- spec/models/matching/engine_spec.rb
+++ spec/models/matching/engine_spec.rb
@@ -1,14 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Matching::Engine, type: :model do
-  # let(:client) { create :client }
-  # let(:must_be_female) { create :female }
-  # let(:eighteen_plus) { create :eighteen_plus }
-  # let(:requirement) { create :requirement, rule: eighteen_plus, positive: true }
-  # let(:program) { create :program, requirements: [requirement] }
-  # let(:sub_program) { create :sub_program, program: program }
-  # let(:voucher) { create :voucher, sub_program: sub_program }
-  # let(:opportunity) { create :opportunity, voucher: voucher }
+  let!(:client) { create :client }  
+  let!(:program_with_all_rules) { 
+    # make sure we have all of the rules
+    CasSeeds::Rules.new.run!
+    requirements = Rule.all.map do |rule|
+      create :requirement, rule: rule, positive: true
+    end
+    create :program, requirements: requirements
+  }
+  let!(:complex_sub_program) { create :sub_program, program: program_with_all_rules }
+  let!(:complex_voucher) { create :voucher, sub_program: complex_sub_program }
+  let!(:complex_opportunity) { create :opportunity, voucher: complex_voucher }
+  let!(:simple_program) {
+    requirement = create :requirement, rule: Rules::AgeGreaterThanEightteen.first, positive: true
+    create :program, requirements: [requirement]
+  }
+  let!(:simple_sub_program) { create :sub_program, program: simple_program }
+  let!(:simple_voucher) { create :voucher, sub_program: simple_sub_program }
+  let!(:simple_opportunity) { create :opportunity, voucher: simple_voucher }
   
   describe "engine_modes" do
     it 'contains VI-SPDAT Priority Score' do
@@ -16,42 +27,19 @@ RSpec.describe Matching::Engine, type: :model do
     end
   end
 
-  # describe "engine runs" do
-  #   it 'and does not throw an error' do
-  #     expect { 
-  #       opp_id = opportunity.id
-  #       client_id = client.id
-  #       # HUD HMIS Data Standards UNIVERSAL DATA ELEMENTS 3.6 Gender
-  #       {
-  #         0 => 'Female',
-  #         1 => 'Male',
-  #         2 => 'Transgender male to female',
-  #         3 => 'Transgender female to male',
-  #         4 => 'Doesn’t identify as male, female, or transgender',
-  #         8 => 'Client doesn’t know',
-  #         9 => 'Client refused',
-  #         99 => 'Data not collected'
-  #       }.each do |id, name|
-  #         Gender.create! do |r|
-  #           r.numeric = id
-  #           r.text = name
-  #         end
-  #       end
-  #       puts Opportunity.first.inspect
-  #       Matching::RunEngineJob.perform_now 
-  #       puts Matching::Engine.available_opportunities.count
-  #       puts Matching::Engine.available_client_count
-  #       engine = Matching::Engine.new(Matching::Engine.available_clients, Matching::Engine.available_opportunities)
-  #       puts engine.clients_for_matches(opportunity).inspect
-  #       # puts Opportunity.first.inspect
-  #       # puts Client.first.inspect
-  #       # puts opportunity.client_opportunity_matches.inspect
-  #       # puts requirement.name
-  #       # puts ClientOpportunityMatch.count
-  #       # puts client.gender.inspect
-  #     }.to_not raise_error
-      
-  #   end
-  # end
+  describe "engine runs" do
+    it 'and does not throw an error' do
+      expect { Matching::RunEngineJob.perform_now }.to_not raise_error
+    end
+    it 'and finds a match' do
+      expect {
+        Matching::RunEngineJob.perform_now
+      }.to change(ClientOpportunityMatch, :count).by(1)
+    end
+    it ' and client match is the same as opportunity active match' do
+      expect(client.client_opportunity_matches.first).to eq(simple_opportunity.active_match)
+    end
+  end
 
 end
+Client.arel_table[:date_of_birth]
