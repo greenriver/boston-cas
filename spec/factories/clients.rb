021205d473dacd8a304fa16d999af7b33806db13
Elliot Anders <eanders@greenriver.org>
2017-11-10 12:48:53 -0500

    Don't use faker name in rspec factories

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :client, class: 'Client' do
     ssn Faker::Number.number(9)
-    first_name Faker::Name.first_name
-    last_name Faker::Name.last_name
+    first_name 'Client'
+    last_name 'Last'
     available true
     available_candidate true
     gender_id 0 # female
