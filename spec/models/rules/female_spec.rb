a466d71abe4fbd2cf45ef4703713beab91227dca
Peter Clark <peter@greenriver.org>
2017-11-27 09:22:16 -0600

    correcting match rules for TMF, TFM

--- spec/models/rules/female_spec.rb
+++ spec/models/rules/female_spec.rb
@@ -20,25 +20,37 @@ RSpec.describe Rules::Female, type: :model do
   describe 'clients_that_fit' do
 
     context 'when positive' do
-      it 'matches female and transgender' do
-        expect( clients_that_fit.count ).to eq 6
+      it 'matches female and TMF' do
+        expect( clients_that_fit.count ).to eq 4
       end
       it 'contains females' do
         expect( clients_that_fit.ids ).to include *female_clients.map(&:id)
       end
-      it 'contains transgender male to female' do
+      it 'contains TMF' do
         expect( clients_that_fit.ids ).to include *trans_mf_clients.map(&:id)
       end
-      it 'contains transgender female to male' do
-        expect( clients_that_fit.ids ).to include *trans_fm_clients.map(&:id)
+      it 'does not contain TFM' do
+        expect( clients_that_fit.ids ).to_not include *trans_fm_clients.map(&:id)
+      end
+      it 'does not contain males' do
+        expect( clients_that_fit.ids ).to_not include *male_clients.map(&:id)
       end
     end
     context 'when negative' do
       it 'matches non-females' do
-        expect( clients_that_dont_fit.count ).to eq 2
+        expect( clients_that_dont_fit.count ).to eq 4
       end
       it 'contains males' do
         expect( clients_that_dont_fit.ids ).to include *male_clients.map(&:id)
+      end
+      it 'contains TFM' do
+        expect( clients_that_dont_fit.ids ).to include *trans_fm_clients.map(&:id)
+      end
+      it 'does not contain females' do
+        expect( clients_that_dont_fit.ids ).to_not include *female_clients.map(&:id)
+      end
+      it 'does not contain TMF' do
+        expect( clients_that_dont_fit.ids ).to_not include *trans_mf_clients.map(&:id)
       end
     end
   end
