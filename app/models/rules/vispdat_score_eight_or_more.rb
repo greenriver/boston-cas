4113524868e0891db657b751b2a64563d891f159
Elliot Anders <eanders@greenriver.org>
2018-01-15 09:14:32 -0500

    VISPDAT score rules

---
+++ app/models/rules/vispdat_score_eight_or_more.rb
@@ -0,0 +1,15 @@
+class Rules::VispdatScoreEightOrMore < Rule
+  def clients_that_fit(scope, requirement)
+    c_t = Client.arel_table
+    if Client.column_names.include?(:vispdat_score.to_s)
+      if requirement.positive
+        where = c_t[:vispdat_score].gteq(8)
+      else
+        where = c_t[:vispdat_score].lt(8)
+      end
+      scope.where(where)
+    else
+      raise RuleDatabaseStructureMissing.new("clients.vispdat_score missing. Cannot check clients against #{self.class}.")
+    end
+  end
+end
---
+++ app/models/rules/vispdat_score_four_to_seven.rb
@@ -0,0 +1,15 @@
+class Rules::VispdatScoreFourToSeven < Rule
+  def clients_that_fit(scope, requirement)
+    c_t = Client.arel_table
+    if Client.column_names.include?(:vispdat_score.to_s)
+      if requirement.positive
+        where = c_t[:vispdat_score].gteq(4).and(c_t[:vispdat_score].lteq(7))
+      else
+        where = c_t[:vispdat_score].lt(4).or(c_t[:vispdat_score].gt(7))
+      end
+      scope.where(where)
+    else
+      raise RuleDatabaseStructureMissing.new("clients.vispdat_score missing. Cannot check clients against #{self.class}.")
+    end
+  end
+end
