f12476497666f061cd6821694df53e287b932bba
Elliot Anders <eanders@greenriver.org>
2018-03-09 08:37:34 -0500

    error correction for voucher availability update

--- app/models/cas/update_voucher_availability.rb
+++ app/models/cas/update_voucher_availability.rb
@@ -5,7 +5,11 @@ module Cas
       vouchers = Voucher.where.not(date_available: nil).where(available: false)
       vouchers.each do |v|
         if v.opportunity.blank? && v.date_available <= Date.today
-          v.update! available: true, date_available: nil
+          begin
+            v.update! available: true, date_available: nil
+          rescue ActiveRecord::RecordInvalid => e
+            Rails.logger.error "Error for voucher: #{v.id} #{e.message}"
+          end
         end
       end
     end
