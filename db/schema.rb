be4cdac1ce95975f21caaa9cee2a53d1f532e9dd
Elliot Anders <eanders@greenriver.org>
2018-05-07 12:50:07 -0400

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180505235920) do
 
   # These are extensions that must be enabled in order to support this database
@@ -192,7 +191,7 @@ ActiveRecord::Schema.define(version: 20180505235920) do
     t.jsonb    "active_cohort_ids"
     t.string   "client_identifier"
     t.integer  "assessment_score",                                  default: 0,     null: false
-
+  end
 
   add_index "clients", ["deleted_at"], name: "index_clients_on_deleted_at", using: :btree
 
@@ -732,7 +731,6 @@ ActiveRecord::Schema.define(version: 20180505235920) do
     t.boolean  "can_edit_all_clients",                    default: false
     t.boolean  "can_participate_in_matches",              default: false
     t.boolean  "can_view_all_matches",                    default: false
-    t.boolean  "can_view_own_closed_matches",             default: false
     t.boolean  "can_see_alternate_matches",               default: false
     t.boolean  "can_edit_match_contacts",                 default: false
     t.boolean  "can_approve_matches",                     default: false
@@ -743,11 +741,6 @@ ActiveRecord::Schema.define(version: 20180505235920) do
     t.boolean  "can_edit_users",                          default: false
     t.boolean  "can_view_full_ssn",                       default: false
     t.boolean  "can_view_full_dob",                       default: false
-    t.boolean  "can_view_dmh_eligibility",                default: false
-    t.boolean  "can_view_va_eligibility",                 default: false
-    t.boolean  "can_view_hues_eligibility",               default: false
-    t.boolean  "can_view_hiv_positive_eligibility",       default: false
-    t.boolean  "can_view_client_confidentiality",         default: false
     t.boolean  "can_view_buildings",                      default: false
     t.boolean  "can_edit_buildings",                      default: false
     t.boolean  "can_view_funding_sources",                default: false
@@ -772,7 +765,13 @@ ActiveRecord::Schema.define(version: 20180505235920) do
     t.boolean  "can_edit_available_services",             default: false
     t.boolean  "can_assign_services",                     default: false
     t.boolean  "can_assign_requirements",                 default: false
+    t.boolean  "can_view_dmh_eligibility",                default: false
+    t.boolean  "can_view_va_eligibility",                 default: false, null: false
+    t.boolean  "can_view_hues_eligibility",               default: false, null: false
     t.boolean  "can_become_other_users",                  default: false
+    t.boolean  "can_view_client_confidentiality",         default: false, null: false
+    t.boolean  "can_view_hiv_positive_eligibility",       default: false
+    t.boolean  "can_view_own_closed_matches",             default: false
     t.boolean  "can_edit_translations",                   default: false
     t.boolean  "can_view_vspdats",                        default: false
     t.boolean  "can_manage_config",                       default: false
