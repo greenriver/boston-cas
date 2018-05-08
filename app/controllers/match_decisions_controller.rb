e930c7df2df5321bb612c31d49ec0acb5814d318
jgoley <jgoley@gmail.com>
2018-05-08 10:32:43 -0400

    Give terms to show of match decisions

--- app/controllers/match_decisions_controller.rb
+++ app/controllers/match_decisions_controller.rb
@@ -14,6 +14,7 @@ class MatchDecisionsController < ApplicationController
     @opportunity = @match.opportunity
     @program = @match.program
     @sub_program = @match.sub_program
+    @types = MatchRoutes::Base.match_steps
     if params[:notification_id].present?
       @notification = @access_context.notification
     end
