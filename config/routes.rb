ccde5c92f8cee21883507908e7035c2876be29ee
Todd Blackman <tdblackman@gmail.com>
2018-06-07 10:23:47 -0400

    Check on cache

--- config/routes.rb
+++ config/routes.rb
@@ -121,9 +121,10 @@ Rails.application.routes.draw do
 
   namespace :system_status do
     get :operational
+    get :cache_status
   end
 
-  
+
   resources :deidentified_clients, only: [:index, :new, :create, :edit, :update, :destroy,]
   resources :messages, only: [:show, :index] do
     collection do
