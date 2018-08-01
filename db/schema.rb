00d51aeaddbdc3deeb2b0d5360bccd38a66d71dc
Elliot Anders <eanders@greenriver.org>
2018-08-01 13:08:12 -0400

    Enable disabling users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180710125552) do
+ActiveRecord::Schema.define(version: 20180801141411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1024,6 +1024,7 @@ ActiveRecord::Schema.define(version: 20180710125552) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email_schedule",               default: "immediate", null: false
+    t.boolean  "active",                       default: true,        null: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
