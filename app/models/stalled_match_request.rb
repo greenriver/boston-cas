3d34a7348fc99926288df764bc4eb0436b3539ca
Elliot Anders <eanders@greenriver.org>
2018-10-01 13:24:52 -0400

    WIP; stub for new stalled match setup

---
+++ app/models/stalled_match_request.rb
@@ -0,0 +1,16 @@
+class StalledMatchRequest < ::ActiveRecord::Base
+  belongs_to :match,
+      class_name: ClientOpportunityMatch.name,
+      inverse_of: :stalled_match_requests
+
+    has_many :notifications,
+      class_name: Notifications::Base.name
+
+    belongs_to :decision,
+      class_name: MatchDecisions::Base.name,
+      inverse_of: :stalled_match_requests
+
+    has_one :match_route, through: :match
+    delegate :stalled_interval, to: :match_route
+
+end
