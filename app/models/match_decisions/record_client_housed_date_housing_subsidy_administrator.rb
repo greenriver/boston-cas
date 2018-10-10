016353937492d1d3a337ed602f87cb0071eb727b
Elliot Anders <eanders@greenriver.org>
2018-10-10 08:50:52 -0400

    Enable match status updates on another step

--- app/models/match_decisions/record_client_housed_date_housing_subsidy_administrator.rb
+++ app/models/match_decisions/record_client_housed_date_housing_subsidy_administrator.rb
@@ -41,6 +41,19 @@ module MatchDecisions
       super && saved_status !~ /complete/
     end
 
+    def stallable?
+      true
+    end
+
+    def stalled_contact_types
+      @stalled_contact_types ||= [
+        :shelter_agency_contacts,
+        :housing_subsidy_admin_contacts,
+        :ssp_contacts,
+        :hsp_contacts,
+      ]
+    end
+
     def permitted_params
       super + [:client_move_in_date]
     end
