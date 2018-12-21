2ddfe411505b7e51f2c5d27f333027c3db9ec0c8
Sandy Wise <sandy@greenriver.org>
2018-12-21 13:51:42 -0500

    Add some type safety to the params

--- app/controllers/neighborhood_interests_controller.rb
+++ app/controllers/neighborhood_interests_controller.rb
@@ -5,13 +5,13 @@ class NeighborhoodInterestsController < ApplicationController
 
   def create
     neighborhood_interests_parms[:neighborhood_id].reject(&:empty?).each do | neighborhood_id |
-      NeighborhoodInterest.create(client_id: @client_id, neighborhood_id: neighborhood_id)
+      NeighborhoodInterest.create(client_id: @client_id, neighborhood_id: neighborhood_id.to_i)
     end
     redirect_to client_path(@client_id)
   end
 
   def destroy
-    interest = NeighborhoodInterest.find(params.require(:id))
+    interest = NeighborhoodInterest.find(params.require(:id).to_i)
     interest.destroy
     redirect_to client_path(@client_id)
   end
@@ -22,6 +22,6 @@ class NeighborhoodInterestsController < ApplicationController
   end
 
   def set_client_id
-    @client_id = params.require(:client_id)
+    @client_id = params.require(:client_id).to_i
   end
 end
