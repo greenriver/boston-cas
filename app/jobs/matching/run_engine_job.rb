4433fc7ea5847c64f4e4d92a02bbd6c7821715c0
Sandy Wise <sandy@greenriver.org>
2018-12-31 10:34:23 -0500

    Only run engine if flag is set

--- app/jobs/matching/run_engine_job.rb
+++ app/jobs/matching/run_engine_job.rb
@@ -4,12 +4,14 @@ module Matching
     def perform
       # prevent the engine from running simultaneously
       ClientOpportunityMatch.with_advisory_lock(:engine_lock) do
-        MatchRoutes::Base.all_routes.each do |route|
-          Matching::Matchability.update(Opportunity.on_route(route), match_route: route)
-          Matching::Engine.create_candidates(match_route: route)
+        # TODO replaced all_routes with available -- this means inactive routes will not be run, I believe this was the intended behavior, anyway?
+        MatchRoutes::Base.available.each do |route|
+          if route.should_run_engine
+            Matching::Matchability.update(Opportunity.on_route(route), match_route: route)
+            Matching::Engine.create_candidates(match_route: route)
+          end
         end
       end
     end
-
   end
 end
