2538536132319042ce31679988eb6b9c10c0eb82
Sandy Wise <sandy@greenriver.org>
2019-01-02 14:17:48 -0500

    Remove completed TODO

--- app/jobs/matching/run_engine_job.rb
+++ app/jobs/matching/run_engine_job.rb
@@ -4,7 +4,6 @@ module Matching
     def perform
       # prevent the engine from running simultaneously
       ClientOpportunityMatch.with_advisory_lock(:engine_lock) do
-        # TODO replaced all_routes with available -- this means inactive routes will not be run, I believe this was the intended behavior, anyway?
         MatchRoutes::Base.available.each do |route|
           Matching::Matchability.update(Opportunity.on_route(route), match_route: route)
           Matching::Engine.create_candidates(match_route: route)
