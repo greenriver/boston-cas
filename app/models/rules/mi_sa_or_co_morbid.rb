137900dd6857b20a7ef7305bc0d5b6cae200b101
Sandy Wise <sandy@greenriver.org>
2019-01-04 13:59:34 -0500

    Update rule to make tests green

--- app/models/rules/mi_sa_or_co_morbid.rb
+++ app/models/rules/mi_sa_or_co_morbid.rb
@@ -2,7 +2,11 @@ class Rules::MiSaOrCoMorbid < Rule
   def clients_that_fit(scope, requirement)
     c_t = Client.arel_table
     if Client.column_names.include?(:mental_health_problem.to_s) && Client.column_names.include?(:substance_abuse_problem.to_s)
-      scope.where(c_t[:mental_health_problem].eq(requirement.positive).or(c_t[:substance_abuse_problem].eq(requirement.positive)))
+      if requirement.positive
+        scope.where(c_t[:mental_health_problem].eq(true).or(c_t[:substance_abuse_problem].eq(true)))
+      else
+        scope.where(mental_health_problem: false, substance_abuse_problem: false)
+      end
     else
       raise RuleDatabaseStructureMissing.new("clients.mental_health_problem or clients.substance_abuse_problem missing. Cannot check clients against #{self.class}.")
     end
