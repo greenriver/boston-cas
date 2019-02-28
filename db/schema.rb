2f106bc17895909e523e092f23669a6b1224fdc6
Sandy Wise <sandy@greenriver.org>
2019-02-28 12:13:19 -0500

    Add interested_in_set_asides to Non-HMIS clients

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190228163002) do
+ActiveRecord::Schema.define(version: 20190228165352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -557,6 +557,7 @@ ActiveRecord::Schema.define(version: 20190228163002) do
     t.date     "date_days_homeless_verified"
     t.string   "who_verified_days_homeless"
     t.boolean  "domestic_violence",                        default: false, null: false
+    t.boolean  "interested_in_set_asides",                 default: false
   end
 
   add_index "non_hmis_clients", ["deleted_at"], name: "index_non_hmis_clients_on_deleted_at", using: :btree
