453a8d767f4dd43d82480c015bc111ee69b27287
Andrew Braunstein <abraunstein@storymd.com>
2019-05-15 11:36:09 -0400

    [#165221419] Added searching for Parked Client(s)

--- app/controllers/reports/parked_clients_controller.rb
+++ app/controllers/reports/parked_clients_controller.rb
@@ -1,17 +1,25 @@
 module Reports
   class ParkedClientsController < ApplicationController
     before_action :authenticate_user!
-  	
+
     def index
-      if can_view_all_clients?
-        @clients = Client.parked.
+      base_scope =
+        if can_view_all_clients?
+          Client.parked
+        else
+          Client.non_confidential.full_release.parked
+        end
+
+      @clients =
+          if params[:q].present?
+            base_scope.text_search(params[:q])
+          else
+            base_scope
+          end
+
+      @clients = @clients.
           order(prevent_matching_until: :asc).
           page(params[:page].to_i).per(25)
-      else
-        @clients = Client.non_confidential.full_release.parked.
-          order(prevent_matching_until: :asc).
-          page(params[:page].to_i).per(25)
-      end
     end
   end
 end
