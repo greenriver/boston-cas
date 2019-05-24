ecea00f0520bb75a3e4636ded43a1a269b25ecc8
Sandy Wise <sandy@greenriver.org>
2019-05-24 15:53:14 -0400

    Search for vouchers by name

--- app/controllers/in_progress_vouchers_controller.rb
+++ app/controllers/in_progress_vouchers_controller.rb
@@ -1,7 +1,8 @@
 class InProgressVouchersController < VouchersController
   def index
     @vouchers = @subprogram.vouchers.preload(:status_match).order(:id)
-    @vouchers_for_page = @vouchers.select{|v| v.status_match.present? && v.status_match.active}
+    @q = @vouchers.ransack(params[:q])
+    @vouchers_for_page = @q.result.preload(:status_match).select{|v| v.status_match.present? && v.status_match.active}
     @voucher_state = "in-progress"
   end
 end
--- app/controllers/successful_vouchers_controller.rb
+++ app/controllers/successful_vouchers_controller.rb
@@ -1,7 +1,8 @@
 class SuccessfulVouchersController < VouchersController
   def index
     @vouchers = @subprogram.vouchers.preload(:status_match).order(:id)
-    @vouchers_for_page = @vouchers.select{|v| v.status_match.present? && v.status_match.successful?}
+    @q = @vouchers.ransack(params[:q])
+    @vouchers_for_page = @q.result.preload(:status_match).select{|v| v.status_match.present? && v.status_match.successful?}
     @voucher_state = "successful"
   end
 end
