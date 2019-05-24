ed4a4e9b763adb8ae6e4842d7447aea3ba2b968d
Sandy Wise <sandy@greenriver.org>
2019-05-24 16:55:53 -0400

    Don't show confidential matches as a result of searching

--- app/controllers/in_progress_vouchers_controller.rb
+++ app/controllers/in_progress_vouchers_controller.rb
@@ -3,6 +3,11 @@ class InProgressVouchersController < VouchersController
     @vouchers = @subprogram.vouchers.preload(:status_match).order(:id)
     @q = @vouchers.ransack(params[:q])
     @vouchers_for_page = @q.result.preload(:status_match).select{|v| v.status_match.present? && v.status_match.active}
-    @voucher_state = "in-progress"
+    if params[:q]&.[](:client_search).present?
+      @vouchers_for_page = @vouchers_for_page.select{|v| v.status_match.present? && !v.status_match.confidential?}
+      @voucher_state = "matching in-progress"
+    else
+      @voucher_state = "in-progress"
+    end
   end
 end
--- app/controllers/successful_vouchers_controller.rb
+++ app/controllers/successful_vouchers_controller.rb
@@ -3,6 +3,11 @@ class SuccessfulVouchersController < VouchersController
     @vouchers = @subprogram.vouchers.preload(:status_match).order(:id)
     @q = @vouchers.ransack(params[:q])
     @vouchers_for_page = @q.result.preload(:status_match).select{|v| v.status_match.present? && v.status_match.successful?}
-    @voucher_state = "successful"
+    if params[:q]&.[](:client_search).present?
+      @vouchers_for_page = @vouchers_for_page.select{|v| v.status_match.present? && !v.status_match.confidential?}
+      @voucher_state = "matching successful"
+    else
+      @voucher_state = "successful"
+    end
   end
 end
