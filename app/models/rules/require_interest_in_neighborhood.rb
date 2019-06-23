d1abcaad05b1e7f6c5c0c2f6ac64e9f53473df3e
Sandy Wise <sandy@greenriver.org>
2019-06-23 15:42:08 -0400

    Add 'have at least one neighborhood preference' rule

---
+++ app/models/rules/require_interest_in_neighborhood.rb
@@ -0,0 +1,20 @@
+##
+# Copyright 2016 - 2019 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/boston-cas/blob/master/LICENSE.md
+###
+
+class Rules::RequireInterestInNeighborhood < Rule
+  def clients_that_fit(scope, requirement)
+    if Client.column_names.include?(:neighborhood_interests.to_s)
+      if requirement.positive
+        where = 'not(neighborhood_interests = \'[]\')'
+      else
+        where = 'neighborhood_interests = \'[]\''
+      end
+      scope.where(where, requirement.variable.to_s)
+    else
+      raise RuleDatabaseStructureMissing.new("clients.neighborhood_interests missing. Cannot check clients against #{self.class}.")
+    end
+  end
+end
