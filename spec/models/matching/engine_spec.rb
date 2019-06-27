35179395131faf9ee0225f4aef702d1b1aa29b41
Elliot Anders <eanders@greenriver.org>
2019-06-27 18:32:55 -0400

    Testing fix for travis failure

--- spec/models/matching/engine_spec.rb
+++ spec/models/matching/engine_spec.rb
@@ -50,7 +50,9 @@ RSpec.describe Matching::Engine, type: :model do
     end
     it ' and client match is the same as opportunity active match' do
       Matching::RunEngineJob.new.perform
-      expect(client.client_opportunity_matches.first).to eq(simple_opportunity.active_matches.first)
+      puts client.inspect
+      puts ClientOpportunityMatch.all.inspect
+      expect(simple_opportunity.active_matches.first).to eq(client.client_opportunity_matches.first)
     end
   end
 end
