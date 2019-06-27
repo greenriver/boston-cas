5089d45b73ad8086bd8793f38eb7021633946ab3
Elliot Anders <eanders@greenriver.org>
2019-06-27 18:54:46 -0400

    Removing debugging, travis test passes when in-place

--- spec/models/matching/engine_spec.rb
+++ spec/models/matching/engine_spec.rb
@@ -50,8 +50,6 @@ RSpec.describe Matching::Engine, type: :model do
     end
     it ' and client match is the same as opportunity active match' do
       Matching::RunEngineJob.new.perform
-      puts client.inspect
-      puts ClientOpportunityMatch.all.inspect
       expect(simple_opportunity.active_matches.first).to eq(client.client_opportunity_matches.first)
     end
   end
