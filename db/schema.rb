55e78d34a2da223c8efb8fb401a7a9af5e6bbaf6
Elliot Anders <eanders@greenriver.org>
2019-07-31 11:38:41 -0400

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -176,7 +176,7 @@ ActiveRecord::Schema.define(version: 20190726125407) do
     t.boolean  "disabling_condition",                                    default: false
     t.datetime "release_of_information"
     t.date     "prevent_matching_until"
-    t.boolean  "dmh_eligible",                                           default: false, null: false
+    t.boolean  "dmh_eligible",                                           default: false
     t.boolean  "va_eligible",                                            default: false, null: false
     t.boolean  "hues_eligible",                                          default: false, null: false
     t.datetime "disability_verified_on"
@@ -201,8 +201,8 @@ ActiveRecord::Schema.define(version: 20190726125407) do
     t.integer  "vispdat_priority_score",                                 default: 0
     t.integer  "vispdat_length_homeless_in_days",                        default: 0,     null: false
     t.boolean  "cspech_eligible",                                        default: false
-    t.date     "calculated_last_homeless_night"
     t.string   "alternate_names"
+    t.date     "calculated_last_homeless_night"
     t.boolean  "congregate_housing",                                     default: false
     t.boolean  "sober_housing",                                          default: false
     t.jsonb    "enrolled_project_ids"
@@ -585,13 +585,13 @@ ActiveRecord::Schema.define(version: 20190726125407) do
     t.integer  "gender"
     t.string   "type"
     t.boolean  "available",                                default: true,  null: false
+    t.date     "date_days_homeless_verified"
+    t.string   "who_verified_days_homeless"
     t.jsonb    "neighborhood_interests",                   default: [],    null: false
     t.float    "income_total_monthly"
     t.boolean  "disabling_condition",                      default: false
     t.boolean  "physical_disability",                      default: false
     t.boolean  "developmental_disability",                 default: false
-    t.date     "date_days_homeless_verified"
-    t.string   "who_verified_days_homeless"
     t.boolean  "domestic_violence",                        default: false, null: false
     t.boolean  "interested_in_set_asides",                 default: false
     t.jsonb    "tags"
@@ -774,7 +774,7 @@ ActiveRecord::Schema.define(version: 20190726125407) do
     t.string   "workphone"
     t.string   "pager"
     t.string   "email"
-    t.boolean  "dmh_eligible",                                default: false, null: false
+    t.boolean  "dmh_eligible",                                default: false
     t.boolean  "va_eligible",                                 default: false, null: false
     t.boolean  "hues_eligible",                               default: false, null: false
     t.datetime "disability_verified_on"
@@ -887,7 +887,6 @@ ActiveRecord::Schema.define(version: 20190726125407) do
     t.boolean  "can_edit_all_clients",                    default: false
     t.boolean  "can_participate_in_matches",              default: false
     t.boolean  "can_view_all_matches",                    default: false
-    t.boolean  "can_view_own_closed_matches",             default: false
     t.boolean  "can_see_alternate_matches",               default: false
     t.boolean  "can_edit_match_contacts",                 default: false
     t.boolean  "can_approve_matches",                     default: false
@@ -898,11 +897,6 @@ ActiveRecord::Schema.define(version: 20190726125407) do
     t.boolean  "can_edit_users",                          default: false
     t.boolean  "can_view_full_ssn",                       default: false
     t.boolean  "can_view_full_dob",                       default: false
-    t.boolean  "can_view_dmh_eligibility",                default: false
-    t.boolean  "can_view_va_eligibility",                 default: false
-    t.boolean  "can_view_hues_eligibility",               default: false
-    t.boolean  "can_view_hiv_positive_eligibility",       default: false
-    t.boolean  "can_view_client_confidentiality",         default: false
     t.boolean  "can_view_buildings",                      default: false
     t.boolean  "can_edit_buildings",                      default: false
     t.boolean  "can_view_funding_sources",                default: false
@@ -927,15 +921,21 @@ ActiveRecord::Schema.define(version: 20190726125407) do
     t.boolean  "can_edit_available_services",             default: false
     t.boolean  "can_assign_services",                     default: false
     t.boolean  "can_assign_requirements",                 default: false
+    t.boolean  "can_view_dmh_eligibility",                default: false
+    t.boolean  "can_view_va_eligibility",                 default: false, null: false
+    t.boolean  "can_view_hues_eligibility",               default: false, null: false
     t.boolean  "can_become_other_users",                  default: false
+    t.boolean  "can_view_client_confidentiality",         default: false, null: false
+    t.boolean  "can_view_hiv_positive_eligibility",       default: false
+    t.boolean  "can_view_own_closed_matches",             default: false
     t.boolean  "can_edit_translations",                   default: false
     t.boolean  "can_view_vspdats",                        default: false
     t.boolean  "can_manage_config",                       default: false
     t.boolean  "can_create_overall_note",                 default: false
-    t.boolean  "can_delete_client_notes",                 default: false
     t.boolean  "can_enter_deidentified_clients",          default: false
     t.boolean  "can_manage_deidentified_clients",         default: false
     t.boolean  "can_add_cohorts_to_deidentified_clients", default: false
+    t.boolean  "can_delete_client_notes",                 default: false
     t.boolean  "can_enter_identified_clients",            default: false
     t.boolean  "can_manage_identified_clients",           default: false
     t.boolean  "can_add_cohorts_to_identified_clients",   default: false
