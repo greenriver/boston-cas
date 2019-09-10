d6582237be94b2657d6fa4a07e048d6f0c544914
Elliot Anders <eanders@greenriver.org>
2019-09-10 13:09:30 -0400

    Attempting to fix test that fails in CI, but passes locally

--- spec/models/matching/engine_spec.rb
+++ spec/models/matching/engine_spec.rb
@@ -50,7 +50,7 @@ RSpec.describe Matching::Engine, type: :model do
     end
     it ' and client match is the same as opportunity active match' do
       Matching::RunEngineJob.new.perform
-      expect(simple_opportunity.active_matches.first).to eq(client.client_opportunity_matches.first)
+      expect(simple_opportunity.active_matches.reload.first).to eq(client.client_opportunity_matches.reload.first)
     end
   end
 end
