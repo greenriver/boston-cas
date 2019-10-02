b578fcfc9edd30efae03e5c941cd18715940bc18
Elliot Anders <eanders@greenriver.org>
2019-10-02 13:48:45 -0400

    Some checks for odd data situations

--- db/migrate/20191001174803_add_match_route_to_matches.rb
+++ db/migrate/20191001174803_add_match_route_to_matches.rb
@@ -1,5 +1,5 @@
 class AddMatchRouteToMatches < ActiveRecord::Migration
   def change
-    add_belongs_to :client_opportunity_matches, :match_route
+    add_column :client_opportunity_matches, :match_route_id, :integer
   end
 end
--- db/migrate/20191001183520_copy_existing_match_routes_into_client_opportunity_matches.rb
+++ db/migrate/20191001183520_copy_existing_match_routes_into_client_opportunity_matches.rb
@@ -1,6 +1,7 @@
 class CopyExistingMatchRoutesIntoClientOpportunityMatches < ActiveRecord::Migration
-  def change
+  def up
     ClientOpportunityMatch.find_each do |match|
+      next unless match.program && match.program.match_route
       match.update!(match_route_id: match.program.match_route.id)
     end
   end
