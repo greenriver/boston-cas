4ffa726844258fb67a4fca4fa03545b738079ebc
Elliot Anders <eanders@greenriver.org>
2019-10-09 09:44:26 -0400

    Set first and last names for contacts

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :contact, class: 'Contact' do
+    sequence(:first_name) {|n| "First_#{n}" }
+    sequence(:last_name) {|n| "Last#{n}" }
     sequence(:email) {|n| "user_#{n}@example.com" }
   end
 end
