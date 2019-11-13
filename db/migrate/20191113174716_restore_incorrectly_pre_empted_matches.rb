786e4ee45879de301aac685d9db5335c5071ba16
Elliot Anders <elliot@greenriver.org>
2019-11-13 12:56:42 -0500

    Limit restored decisions to known decisions

--- db/migrate/20191113174716_restore_incorrectly_pre_empted_matches.rb
+++ db/migrate/20191113174716_restore_incorrectly_pre_empted_matches.rb
@@ -1,5 +1,6 @@
 class RestoreIncorrectlyPreEmptedMatches < ActiveRecord::Migration
   def change
+    available_decisions = MatchRoutes::Base.all_routes.flat_map{|r| r.match_steps_for_reporting.keys }
     ClientOpportunityMatch.successful.find_each do |match|
       o = match.opportunity
       puts "Restoring #{o.closed_matches.only_deleted.count} matches related to match id: #{match.id}"
@@ -7,7 +8,7 @@ class RestoreIncorrectlyPreEmptedMatches < ActiveRecord::Migration
         m.restore
         m.contacts.only_deleted.each(&:restore)
         m.events.only_deleted.each(&:restore)
-        m.decisions.only_deleted.each(&:restore)
+        m.decisions.only_deleted.where(type: available_decisions).each(&:restore)
         m.update(match_route_id: m.program.match_route_id) if m.match_route_id.blank?
       end
     end
