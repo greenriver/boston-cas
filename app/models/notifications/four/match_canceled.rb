7d2e1b2e9aae782695079f474e67cd9deba593db
Sandy Wise <sandy@greenriver.org>
2019-11-14 12:01:11 -0500

    Inherit from default route instead of replicate

--- app/models/notifications/four/match_canceled.rb
+++ app/models/notifications/four/match_canceled.rb
@@ -5,20 +5,7 @@
 ###
 
 module Notifications::Four
-  class MatchCanceled < Base
-    
-    def self.create_for_match! match
-      contacts = match.contacts - match.dnd_staff_contacts
-      # don't send to the HSA, SSP, or HSP contacts unless they have been involved
-      contacts -= (match.housing_subsidy_admin_contacts + match.ssp_contacts + match.hsp_contacts) unless match.hsa_involved? 
+  class MatchCanceled < Notifications::MatchCanceled
 
-      contacts.each do |contact|
-        create! match: match, recipient: contact
-      end
-    end
-    
-    def event_label
-      'Contact notified, match canceled'
-    end
   end
 end
