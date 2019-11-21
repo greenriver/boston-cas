b2ac63e3c6e40f0e4a5f30f095c6f68df5038803
Elliot Anders <elliot@greenriver.org>
2019-11-21 16:13:48 -0500

    Show why someone may be available and unavailable at the same time

--- app/models/unavailable_as_candidate_for.rb
+++ app/models/unavailable_as_candidate_for.rb
@@ -4,11 +4,12 @@
 # License detail: https://github.com/greenriver/boston-cas/blob/master/LICENSE.md
 ###
 
-# This is used to determine whether or not a client is available to match a 
+# This is used to determine whether or not a client is available to match a
 # specific match route, existence of a record in this table for a given route
 # means the client is unavailable.
 class UnavailableAsCandidateFor < ActiveRecord::Base
   belongs_to :client
+  belongs_to :route, class_name: 'MatchRoutes::Base', primary_key: :type, foreign_key: :match_route_type
 
   scope :for_route, -> (route) do
     route_name = MatchRoutes::Base.route_name_from_route(route)
