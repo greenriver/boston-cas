8083937ee6b1de196cce4e19e3f4a8a2d092db9c
Elliot Anders <elliot@greenriver.org>
2019-12-04 15:32:07 -0500

    Cache matchability calculation

--- app/models/matching/matchability.rb
+++ app/models/matching/matchability.rb
@@ -6,25 +6,33 @@
 
 module Matching
   class Matchability
-    def self.update(opportunities, match_route:)
-      engine = Engine.for_available_clients(opportunities, match_route: match_route)
-      opportunities.each {|opportunity| new(opportunity, engine).update}
-    end
-
-    def initialize(opportunity, engine)
+    def initialize(opportunity, engine, available_clients_on_route_count)
       @opportunity = opportunity
       @engine = engine
+      @available_clients_on_route_count = available_clients_on_route_count
+    end
+
+    def self.update(opportunities, match_route:)
+      engine = Engine.for_available_clients(opportunities, match_route: match_route)
+      clients_on_route = available_clients_on_route_count(match_route)
+      opportunities.each do |opportunity|
+        new(opportunity, engine, clients_on_route).update
+      end
+    end
+
+    def self.available_clients_on_route_count match_route
+      Matching::Engine.available_client_count(match_route: match_route)
     end
 
     def update
       @opportunity.update_attribute(:matchability, current_value) if @opportunity.matchability != current_value
     end
 
-    def current_value
-      matching_client_count.to_f / Matching::Engine.available_client_count(match_route: @opportunity.match_route)
+    private def current_value
+      matching_client_count.to_f / @available_clients_on_route_count
     end
 
-    def matching_client_count
+    private def matching_client_count
       @opportunity.matching_co_candidates(Client.all).count
     end
   end
