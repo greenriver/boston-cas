82a007658f67fbfb4970a58eb35e6642269dae23
Sandy Wise <sandy@greenriver.org>
2019-12-18 15:26:14 -0500

    Update dev/test gems

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,6 +26,14 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
