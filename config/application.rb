25ad2dd2ee7ebda3f7b21280ea200ba8a41e6b04
Sandy Wise <sandy@greenriver.org>
2019-12-30 13:13:40 -0500

    Move framework defaults to Rails 6.0

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module BostonCa
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults '6.0'
 
     # FIXME Suppress the Rails 5 belongs_to requirement
     Rails.application.config.active_record.belongs_to_required_by_default = false
