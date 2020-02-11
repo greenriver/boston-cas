22216889a896fff7f9e6cf459b09d231669fa5d1
Elliot Anders <elliot@greenriver.org>
2020-02-11 14:57:48 -0500

    Using Uglifier Harmony for asset compilation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
