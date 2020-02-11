71b1892cefccbd9e13c88ec72983f03446970fc6
Elliot Anders <elliot@greenriver.org>
2020-02-11 15:00:11 -0500

    Renaming file to match model name

--- app/helpers/identified_client_helper.rb
+++
@@ -1,9 +0,0 @@
-###
-# Copyright 2016 - 2020 Green River Data Analysis, LLC
-#
-# License detail: https://github.com/greenriver/boston-cas/blob/master/LICENSE.md
-###
-
-module IdentifiedClientsHelper
-  extend NonHmisClientsHelper
-end
