6b02b2aa8d2f40d8fc66a5006af549d9bfa5e456
Sandy Wise <sandy@greenriver.org>
2020-02-26 14:02:35 -0500

    Undo change to call seeds

--- db/migrate/20200221185223_add_parked_clients_report.rb
+++ db/migrate/20200221185223_add_parked_clients_report.rb
@@ -1,5 +1,35 @@
 class AddParkedClientsReport < ActiveRecord::Migration[6.0]
-  def change
-    Rake::Task["db:seed"].invoke
+  REPORTS = {
+    'Operational Reports' => [
+      {
+        url: 'reports/parked_clients',
+        name: 'Parked Clients',
+        description: 'The currently parked clients.',
+        limitable: false,
+      },
+    ],
+  }
+
+
+  def up
+    REPORTS.each do |group, reports|
+      reports.each do |report|
+        ReportDefinition.create(
+          report_group: group,
+          url: report[:url],
+          name: report[:name],
+          description: report[:description],
+          limitable: report[:limitable]
+        )
+      end
+    end
+  end
+
+  def down
+    REPORTS.each do |group, reports|
+      reports.each do |report|
+        ReportDefinition.where(url: report[:url]).delete_all
+      end
+    end
   end
 end
