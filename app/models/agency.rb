293b42d0978a81865d12071f79b44e781ac2bde7
Sandy Wise <sandy@greenriver.org>
2020-02-26 16:50:20 -0500

    Show associated programs on agency index

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -6,10 +6,15 @@
 
 class Agency < ApplicationRecord
   has_many :users, dependent: :nullify
+  has_many :entity_view_permissions
 
   scope :text_search, -> (text) do
     return none unless text.present?
 
     where(arel_table[:name].lower.matches("%#{text.downcase}%"))
+  end
+
+  def program_names
+    Program.editable_by_agency(self).order(:name).pluck(:name).join(', ')
   end
 end
