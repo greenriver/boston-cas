eb50716528ffe6f76a63525cc0e0d39d15d3a908
Sandy Wise <sandy@greenriver.org>
2020-02-28 13:04:26 -0500

    Clone warehouse cas_report table into warehouse

--- app/controllers/concerns/arel_helper.rb
+++ app/controllers/concerns/arel_helper.rb
@@ -46,4 +46,8 @@ module ArelHelper
   def o_t
     Opportunity.arel_table
   end
+
+  def md_b_t
+    MatchDecisions::Base.arel_table
+  end
 end
---
+++ app/models/reporting.rb
@@ -0,0 +1,11 @@
+###
+# Copyright 2016 - 2020 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/boston-cas/blob/master/LICENSE.md
+###
+
+module Reporting
+  def self.table_name_prefix
+    'reporting_'
+  end
+end
