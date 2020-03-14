6116bb2088444cf863ecb47374a233aa2c7b071e
Elliot Anders <elliot@greenriver.org>
2020-03-14 15:58:45 -0400

    Fixes for testing

--- app/models/rules/pathways_eligible.rb
+++ app/models/rules/pathways_eligible.rb
@@ -26,28 +26,24 @@ class Rules::PathwaysEligible < Rule
     # You must not have had a decline reason after the collection date
     # You can have an assessment collection date but not a decline reason
 
-    # Ineligible clients have an appropriate decline that is newer than any
-    # assessment
-    ineligible_ids = ClientOpportunityMatch.closed.
-      on_route(route(requirement.variable)).
-      joins(:client, decisions: :decline_reason).
-      merge(MatchDecisionReasons::Base.ineligible_in_warehouse).
-      merge(Client.where(md_t[:updated_at].gt(c_t[:rrh_assessment_collected_at]))).
-      select(:client_id)
-
-    # client_ids = Client.where.not(rrh_assessment_collected_at: nil).
-    #   left_outer_joins(client_opportunity_matches: [{decisions: :decline_reason}]).
-    #   merge(ClientOpportunityMatch.where(closed: [true, nil])).
-    #   merge(MatchDecisionReasons::Base.where(ineligible_in_warehouse: [true, nil])).
-    #   where(
-    #     md_t[:updated_at].lt(c_t[:rrh_assessment_collected_at]).
-    #     and(mdr_t[:ineligible_in_warehouse])
-    #   ).select(:id)
 
     if requirement.positive
-      scope.where.not(id: ineligible_ids)
+      scope.where.not(id: ineligible_ids(requirement))
     else
-      scope.where(id: ineligible_ids)
+      scope.where(id: ineligible_ids(requirement))
     end
+  end
+
+  # Ineligible clients have an appropriate decline that is newer than any
+    # assessment
+  private def ineligible_ids(requirement)
+    Client.joins(client_opportunity_matches: [{decisions: :decline_reason}]).
+      where(md_t[:updated_at].gt(c_t[:rrh_assessment_collected_at])).
+      merge(
+        ClientOpportunityMatch.closed.
+        on_route(route(requirement.variable))
+      ).
+      merge(MatchDecisionReasons::Base.ineligible_in_warehouse).
+      select(:id)
   end
 end
