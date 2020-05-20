d692caa423fa6191d69c2312d67d99104eea028d
Elliot Anders <elliot@greenriver.org>
2020-05-20 12:55:16 -0400

    Tests for date barriers

--- spec/models/rules/seen_in_last_sixty_days_spec.rb
+++ spec/models/rules/seen_in_last_sixty_days_spec.rb
@@ -1,39 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Rules::SeenInLastSixtyDays, type: :model do
+  let!(:rule) { create :seen_in_last_sixty_days }
+
+  let!(:bob) { create :client, first_name: 'Bob', calculated_last_homeless_night: 61.days.ago.to_date }
+  let!(:roy) { create :client, first_name: 'Roy', calculated_last_homeless_night: 50.days.ago.to_date }
+  let!(:sue) { create :client, first_name: 'Sue', calculated_last_homeless_night: 60.days.ago }
+
+  let!(:positive) { create :requirement, rule: rule, positive: true }
+  let!(:negative) { create :requirement, rule: rule, positive: false }
+
+  let!(:clients_that_fit) { positive.clients_that_fit(Client.all) }
+  let!(:clients_that_dont_fit) { negative.clients_that_fit(Client.all) }
+
   describe 'clients_that_fit' do
-    let!(:rule) { create :seen_in_last_sixty_days }
-
-    let!(:bob) { create :client, first_name: 'Bob', calculated_last_homeless_night: 59.days.ago.to_date }
-    let!(:roy) { create :client, first_name: 'Roy', calculated_last_homeless_night: 61.days.ago.to_date }
-
-    let!(:positive) { create :requirement, rule: rule, positive: true }
-    let!(:negative) { create :requirement, rule: rule, positive: false }
-
-    let!(:clients_that_fit) { positive.clients_that_fit(Client.all) }
-    let!(:clients_that_dont_fit) { negative.clients_that_fit(Client.all) }
-
     context 'when positive' do
-      it 'matches 1' do
-        expect(clients_that_fit.count).to eq(1)
-      end
-      it 'contains Bob' do
-        expect(clients_that_fit.ids).to include bob.id
-      end
-      it 'does not contain Roy' do
-        expect(clients_that_fit.ids).to_not include roy.id
-      end
-    end
-
-    context 'when negative' do
-      it 'matches 1' do
-        expect(clients_that_dont_fit.count).to eq(1)
-      end
-      it 'does not contain Bob' do
-        expect(clients_that_dont_fit.ids).to_not include bob.id
+      it 'matches 2' do
+        expect( clients_that_fit.count ).to eq 2
       end
       it 'contains Roy' do
-        expect(clients_that_dont_fit.ids).to include roy.id
+        expect( clients_that_fit.ids ).to include roy.id
+      end
+      it 'does not contain Bob' do
+        expect( clients_that_fit.ids ).to_not include bob.id
+      end
+      it 'contains Sue' do
+        expect( clients_that_fit.ids ).to include sue.id
+      end
+    end
+    context 'when negative' do
+      it 'matches 1' do
+        expect(clients_that_dont_fit.count).to eq 1
+      end
+      it 'contains Bob' do
+        expect( clients_that_dont_fit.ids ).to include bob.id
+      end
+      it 'does not contain Roy' do
+        expect( clients_that_dont_fit.ids ).to_not include roy.id
+      end
+      it 'does not contain Sue' do
+        expect( clients_that_dont_fit.ids ).to_not include sue.id
       end
     end
   end
