8b32aa39f28359640b0b0fcee5df61b07560021f
Elliot Anders <elliot@greenriver.org>
2020-05-29 16:11:36 -0400

    Account spec changes for new form

--- spec/features/accounts_spec.rb
+++ spec/features/accounts_spec.rb
@@ -1,34 +1,31 @@
 require 'rails_helper'
 
-RSpec.feature "Accounts", type: :feature do
-
+RSpec.feature 'Accounts', type: :feature do
   let(:user) { create(:user) }
 
   before(:each) do
-    visit root_path
+    visit new_user_session_path
   end
 
   feature 'Logging In' do
-    scenario "with wrong password" do
-      click_link 'Sign In'
+    scenario 'with wrong password' do
       fill_in 'Email', with: 'noreply@example.com'
       fill_in 'Password', with: 'password'
       click_button 'Log in'
-      expect( page ).to have_content 'Invalid Email or password'
+      expect(page).to have_content 'Invalid Email or password'
     end
 
-    scenario "with correct password" do
-      click_link 'Sign In'
+    scenario 'with correct password' do
       fill_in 'Email', with: user.email
       fill_in 'Password', with: user.password
       click_button 'Log in'
-      expect( page ).to have_content 'You have successfully signed in.'
+      expect(page).to have_content 'Sign Out'
     end
 
-    feature "Devise lockable" do
-      scenario "account locks after maximum failed attempts" do
+    feature 'Devise lockable' do
+      scenario 'account locks after maximum failed attempts' do
         # Account should lock after a certain number of failed attempts
-        click_link 'Sign In'
+
         Devise.maximum_attempts.times do
           fill_in 'Email', with: user.email
           fill_in 'Password', with: 'password'
@@ -37,12 +34,12 @@ RSpec.feature "Accounts", type: :feature do
         fill_in 'Email', with: user.email
         fill_in 'Password', with: user.password
         click_button 'Log in'
-        expect( page ).to have_content  'Your account is locked.'
+        expect(page).to have_content 'Your account is locked.'
       end
 
-      scenario "account remains locked up until the lockout time is reached" do
+      scenario 'account remains locked up until the lockout time is reached' do
         # Jump forward the to just before the account should be unlocked, it should still be locked
-        click_link 'Sign In'
+
         Devise.maximum_attempts.times do
           fill_in 'Email', with: user.email
           fill_in 'Password', with: 'password'
@@ -52,13 +49,13 @@ RSpec.feature "Accounts", type: :feature do
           fill_in 'Email', with: user.email
           fill_in 'Password', with: user.password
           click_button 'Log in'
-          expect( page ).to have_content  'Your account is locked.'
+          expect(page).to have_content 'Your account is locked.'
         end
       end
 
-      scenario "account is unlocked after time passes" do
+      scenario 'account is unlocked after time passes' do
         # Jump forward the necessary amount of time and verify that the account is unlocked
-        click_link 'Sign In'
+
         Devise.maximum_attempts.times do
           fill_in 'Email', with: user.email
           fill_in 'Password', with: 'password'
@@ -68,10 +65,9 @@ RSpec.feature "Accounts", type: :feature do
           fill_in 'Email', with: user.email
           fill_in 'Password', with: user.password
           click_button 'Log in'
-          expect( page ).to have_content  'You have successfully signed in.'
+          expect(page).to have_content 'Sign Out'
         end
       end
     end
   end
-
 end
