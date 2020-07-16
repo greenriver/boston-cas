9034ed760a01db769f21ed117d6a57340ae413a7
Elliot Anders <elliot@greenriver.org>
2020-07-16 18:21:52 -0400

    Seed CAS db on deploy

--- config/deploy/docker/assets/deploy_tasks.open-path-cas.sh
+++ config/deploy/docker/assets/deploy_tasks.open-path-cas.sh
@@ -23,5 +23,8 @@ bundle exec rake cas_seeds:ensure_all_match_prioritization_schemes_exist
 echo bundle exec rake cas_seeds:stalled_reasons
 bundle exec rake cas_seeds:stalled_reasons
 
+echo general seeding
+bundle exec rake db:seed
+
 echo installing cron
 ./bin/cron_installer.rb
