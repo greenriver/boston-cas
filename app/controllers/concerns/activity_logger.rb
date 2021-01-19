c04e25e3d89066d0d6094d77c9e3f5a377126f7c
Elliot Anders <elliot@greenriver.org>
2021-01-19 09:21:55 -0500

    Formatting

--- app/controllers/concerns/activity_logger.rb
+++ app/controllers/concerns/activity_logger.rb
@@ -24,75 +24,74 @@
 # end
 #
 module ActivityLogger
-    extend ActiveSupport::Concern
-  
-    included do
-      def compose_activity
-        attrs = {
-          user_id: true_user.try(:id),
-          controller_name: params[:controller],
-          ip_address: request.remote_ip,
-          action_name: action_name,
-          item_id: params[:id].presence,
-          referrer: request.referer,
-          session_hash: session.id,
-          method: request.method,
-          path: request.fullpath,
-        }
-        @activity = ActivityLog.new(attrs)
+  extend ActiveSupport::Concern
+
+  included do
+    def compose_activity
+      attrs = {
+        user_id: true_user.try(:id),
+        controller_name: params[:controller],
+        ip_address: request.remote_ip,
+        action_name: action_name,
+        item_id: params[:id].presence,
+        referrer: request.referer,
+        session_hash: session.id,
+        method: request.method,
+        path: request.fullpath,
+      }
+      @activity = ActivityLog.new(attrs)
+    end
+
+    def log_item(item)
+      @activity.item_model = item.class.name
+      @activity.item_id ||= item.id
+    end
+
+    def log_activity
+      @activity.title = begin
+        send("title_for_#{@activity.action_name}")
+      rescue StandardError
+        nil
       end
-  
-      def log_item(item)
-        @activity.item_model = item.class.name
-        @activity.item_id ||= item.id
-      end
-  
-      def log_activity
-        @activity.title = begin
-          send("title_for_#{@activity.action_name}")
-        rescue StandardError
-          nil
-        end
-        @activity.save if @activity.user_id.present?
-      end
-  
-      # override as necessary in the controller
-      protected def title_for_show
-        return @client.name if @client.present?
-        return @user.name if @user.present?
-      end
-  
-      protected def title_for_new
-        title_for_show
-      end
-  
-      protected def title_for_edit
-        title_for_show
-      end
-  
-      protected def title_for_update
-        title_for_show
-      end
-  
-      protected def title_for_create
-        title_for_show
-      end
-  
-      protected def title_for_destroy
-        title_for_show
-      end
-  
-      protected def title_for_index
-        title_for_show
-      end
-  
-      protected def log_client
-        log_item(@client) if @client.present?
-      end
-  
-      protected def log_user
-        log_item(@user) if @user.present?
-      end
+      @activity.save if @activity.user_id.present?
+    end
+
+    # override as necessary in the controller
+    protected def title_for_show
+      return @client.name if @client.present?
+      return @user.name if @user.present?
+    end
+
+    protected def title_for_new
+      title_for_show
+    end
+
+    protected def title_for_edit
+      title_for_show
+    end
+
+    protected def title_for_update
+      title_for_show
+    end
+
+    protected def title_for_create
+      title_for_show
+    end
+
+    protected def title_for_destroy
+      title_for_show
+    end
+
+    protected def title_for_index
+      title_for_show
+    end
+
+    protected def log_client
+      log_item(@client) if @client.present?
+    end
+
+    protected def log_user
+      log_item(@user) if @user.present?
     end
   end
-  
+end
