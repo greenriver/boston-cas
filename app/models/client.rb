ab44d3a7dbc7b2f2dc1585f2670bad905d165bc5
HxHexa <tquang1708@gmail.com>
2021-01-27 10:25:05 -0500

    change specs to pass tests

--- app/models/client.rb
+++ app/models/client.rb
@@ -19,7 +19,7 @@ class Client < ApplicationRecord
   belongs_to :veteran_status, foreign_key: :veteran_status_id, primary_key: :numeric
   belongs_to :gender, foreign_key: :gender_id, primary_key: :numeric
 
-  has_one :project_client, primary_key: :id, foreign_key: :client_id
+  belongs_to :project_client, primary_key: :client_id, foreign_key: :id, optional: true
   has_many :building_clients
 
   # all contacts
