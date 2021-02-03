14820b873edca0a0051bc5af282e6887d28598dd
Elliot Anders <elliot@greenriver.org>
2021-02-03 10:57:16 -0500

    Show a confirmation dialog on voucher submit if the string has been translated.

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -51,6 +51,16 @@ class Voucher < ApplicationRecord
     [unit] + unit.building.units_for_vouchers.to_a
   end
 
+  def self.confirmation_text
+    # NOTE: this must match the string below, but because of the translation
+    # engine's fickleness seems to need to be kept separate.
+    'Please confirm you intend to make vouchers active'
+  end
+
+  def self.translated_confirmation_text
+    _('Please confirm you intend to make vouchers active')
+  end
+
   def self.text_search(text)
     return none unless text.present?
 
--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -2,7 +2,7 @@
 # I18n::InvalidLocale: :en is not a valid locale
 # that probably means your database isn't fully set up, and you may
 # need to comment out all Gettext gems in the Gemfile
-# and remove this initializer temporarily 
+# and remove this initializer temporarily
 # db access is cached <-> only first lookup hits the db
 def database_exists?
   ActiveRecord::Base.connection
@@ -14,6 +14,7 @@ end
 if database_exists? && ActiveRecord::Base.connection.table_exists?('translation_keys')
   require 'gettext_i18n_rails'
   require 'fast_gettext'
+  require 'gettext'
   require "fast_gettext/translation_repository/db"
   FastGettext::TranslationRepository::Db.require_models #load and include default models
 
