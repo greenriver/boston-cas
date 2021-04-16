f08d77754358224583dee5982d1b893a1a505abd
Elliot Anders <elliot@greenriver.org>
2021-04-16 13:29:59 -0400

    Use up not change

--- db/migrate/20210416133245_copy_agency_id_into_assessments.rb
+++ db/migrate/20210416133245_copy_agency_id_into_assessments.rb
@@ -1,5 +1,5 @@
 class CopyAgencyIdIntoAssessments < ActiveRecord::Migration[6.0]
-  def change
+  def up
     NonHmisAssessment.find_each do |assessment|
       assessment.update!(agency_id: assessment.non_hmis_client.agency_id)
     end
