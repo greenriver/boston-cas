15049401a349df162076741e32cc0f8e86953c89
Sandy Wise <sandy@greenriver.org>
2021-04-16 14:20:57 -0400

    Use update column to avoid setting updated_at

--- db/migrate/20210416133245_copy_agency_id_into_assessments.rb
+++ db/migrate/20210416133245_copy_agency_id_into_assessments.rb
@@ -2,7 +2,7 @@ class CopyAgencyIdIntoAssessments < ActiveRecord::Migration[6.0]
   def up
     NonHmisAssessment.find_each do |assessment|
       agency_id = assessment.non_hmis_client&.agency_id
-      assessment.update!(agency_id: agency_id) if agency_id
+      assessment.update_column(:agency_id, agency_id) if agency_id
     end
   end
 end
