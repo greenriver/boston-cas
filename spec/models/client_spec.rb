4cae2c270566835ce7b40aaf8beab4e7fd8e72d6
Elliot Anders <elliot@greenriver.org>
2021-10-27 14:48:36 -0400

    Rubocop Fixups

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,29 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
-
   describe 'prioritized' do
     let!(:clients) { create_list :client, 5 }
     context 'when prioritized by VispdatPriorityScore' do
       let(:priority) { create :priority_vispdat_priority }
       let(:route) { create :default_route, match_prioritization: priority }
       it 'is an ActiveRecord::Relation' do
-        expect( Client.prioritized(route, Client.all) ).to be_an ActiveRecord::Relation
+        expect(Client.prioritized(route, Client.all)).to be_an ActiveRecord::Relation
       end
       it 'orders by vispdat_priority_score' do
-        clients.first(2).map {|m| m.update(vispdat_priority_score: nil)}
-        expect( Client.prioritized(route, Client.all) ).to eq Client.where.not(vispdat_priority_score: nil).order(vispdat_priority_score: :desc)
+        clients.first(2).map { |m| m.update(vispdat_priority_score: nil) }
+        expect(Client.prioritized(route, Client.all)).to eq Client.where.not(vispdat_priority_score: nil).order(vispdat_priority_score: :desc)
       end
     end
     context 'when prioritized by VispdatScore' do
       let(:priority) { create :priority_vispdat }
       let(:route) { create :default_route, match_prioritization: priority }
       it 'is an ActiveRecord::Relation' do
-        expect( Client.prioritized(route, Client.all) ).to be_an ActiveRecord::Relation
+        expect(Client.prioritized(route, Client.all)).to be_an ActiveRecord::Relation
       end
       it 'orders by vispdat_score' do
-        clients.first(2).map {|m| m.update(vispdat_score: nil)}
-        expect( Client.prioritized(route, Client.all) ).to eq Client.where.not(vispdat_score: nil).order(vispdat_score: :desc)
+        clients.first(2).map { |m| m.update(vispdat_score: nil) }
+        expect(Client.prioritized(route, Client.all)).to eq Client.where.not(vispdat_score: nil).order(vispdat_score: :desc)
       end
     end
 
@@ -31,10 +30,10 @@ RSpec.describe Client, type: :model do
       let(:priority) { create :priority_days_homeless_last_three_years }
       let(:route) { create :default_route, match_prioritization: priority }
       it 'is an ActiveRecord::Relation' do
-        expect( Client.prioritized(route, Client.all) ).to be_an ActiveRecord::Relation
+        expect(Client.prioritized(route, Client.all)).to be_an ActiveRecord::Relation
       end
       it 'orders by days_homeless_in_last_three_years' do
-        expect( Client.prioritized(route, Client.all) ).to eq Client.order(days_homeless_in_last_three_years: :desc)
+        expect(Client.prioritized(route, Client.all)).to eq Client.order(days_homeless_in_last_three_years: :desc)
       end
     end
 
@@ -42,10 +41,10 @@ RSpec.describe Client, type: :model do
       let(:priority) { create :priority_days_homeless }
       let(:route) { create :default_route, match_prioritization: priority }
       it 'is an ActiveRecord::Relation' do
-        expect( Client.prioritized(route, Client.all) ).to be_an ActiveRecord::Relation
+        expect(Client.prioritized(route, Client.all)).to be_an ActiveRecord::Relation
       end
       it 'orders by days_homeless' do
-        expect( Client.prioritized(route, Client.all) ).to eq Client.order(days_homeless: :desc)
+        expect(Client.prioritized(route, Client.all)).to eq Client.order(days_homeless: :desc)
       end
     end
 
@@ -53,10 +52,10 @@ RSpec.describe Client, type: :model do
       let(:priority) { create :priority_assessment_score }
       let(:route) { create :default_route, match_prioritization: priority }
       it 'is an ActiveRecord::Relation' do
-        expect( Client.prioritized(route, Client.all) ).to be_an ActiveRecord::Relation
+        expect(Client.prioritized(route, Client.all)).to be_an ActiveRecord::Relation
       end
       it 'orders by assessment_score' do
-        expect( Client.prioritized(route, Client.all).pluck(:id, :assessment_score, :days_homeless) ).to eq Client.order(assessment_score: :desc, rrh_assessment_collected_at: :desc).pluck(:id, :assessment_score, :days_homeless)
+        expect(Client.prioritized(route, Client.all).pluck(:id, :assessment_score, :days_homeless)).to eq Client.order(assessment_score: :desc, rrh_assessment_collected_at: :desc).pluck(:id, :assessment_score, :days_homeless)
       end
     end
 
@@ -110,13 +109,13 @@ RSpec.describe Client, type: :model do
     end
     context 'when searching Bob' do
       it 'matches one' do
-        expect( match_bob.count ).to eq 1
+        expect(match_bob.count).to eq 1
       end
       it 'matches Bob' do
-        expect( match_bob ).to include bob_smith
+        expect(match_bob).to include bob_smith
       end
       it 'does not match Joe Smith' do
-        expect( match_bob ).to_not include joe_smith, billybob_desmith
+        expect(match_bob).to_not include joe_smith, billybob_desmith
       end
     end
   end
@@ -129,16 +128,16 @@ RSpec.describe Client, type: :model do
     end
     context 'when searching Smith' do
       it 'matches 2' do
-        expect( match_smith.count ).to eq 2
+        expect(match_smith.count).to eq 2
       end
       it 'matches Bob' do
-        expect( match_smith ).to include bob_smith
+        expect(match_smith).to include bob_smith
       end
       it 'matches Joe' do
-        expect( match_smith ).to include joe_smith
+        expect(match_smith).to include joe_smith
       end
       it 'does not match either Rays' do
-        expect( match_smith ).to_not include ray_jones, ray_clark, billybob_desmith
+        expect(match_smith).to_not include ray_jones, ray_clark, billybob_desmith
       end
     end
   end
@@ -151,13 +150,13 @@ RSpec.describe Client, type: :model do
     end
     context 'when searching Jones' do
       it 'matches 1' do
-        expect( match_alternate_jones.count ).to eq 1
+        expect(match_alternate_jones.count).to eq 1
       end
       it 'matches Ben' do
-        expect( match_alternate_jones ).to include ben_chris
+        expect(match_alternate_jones).to include ben_chris
       end
       it 'does not match anyone else' do
-        expect( match_alternate_jones ).to_not include *(clients - [ben_chris])
+        expect(match_alternate_jones).to_not include(*(clients - [ben_chris]))
       end
     end
   end
@@ -168,26 +167,28 @@ RSpec.describe Client, type: :model do
     end
     context 'when searching Ray' do
       it 'matches three (two in first name, one in alternate names' do
-        expect( match_ray.count ).to eq 3
+        expect(match_ray.count).to eq 3
       end
       it 'matches Ray' do
-        expect( match_ray ).to include ray_jones
+        expect(match_ray).to include ray_jones
       end
       it 'does not match Joe Smith' do
-        expect( match_ray ).to_not include joe_smith
+        expect(match_ray).to_not include joe_smith
       end
     end
   end
 
   let(:ray_search) do
     Client.where(
-      Client.search_first_name('Ray')
-      .or(Client.search_alternate_name('Ray')))
+      Client.search_first_name('Ray').
+      or(Client.search_alternate_name('Ray')),
+    )
   end
   let(:bob_search) do
     Client.where(
-      Client.search_first_name('Bob')
-      .or(Client.search_alternate_name('Bob')))
+      Client.search_first_name('Bob').
+      or(Client.search_alternate_name('Bob')),
+    )
   end
 
   describe 'combo match on first & alternate' do
@@ -196,14 +197,13 @@ RSpec.describe Client, type: :model do
     end
     context 'when searching Ray' do
       it 'matches 3' do
-        expect( ray_search.count ).to eq 3
+        expect(ray_search.count).to eq 3
       end
     end
     context 'when searching Bob' do
       it 'matches 2' do
-        expect( bob_search.count ).to eq 2
+        expect(bob_search.count).to eq 2
       end
     end
   end
-
 end
