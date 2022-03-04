05c08e1e4c9d74a69f104ba2a6f7aad6fff9e714
Elliot Anders <elliot@greenriver.org>
2022-03-04 09:59:22 -0800

    Nulls last on sort order

--- app/models/match_prioritization/days_homeless_populations.rb
+++ app/models/match_prioritization/days_homeless_populations.rb
@@ -11,7 +11,13 @@ module MatchPrioritization
     end
 
     def self.prioritization_for_clients(scope, match_route:) # rubocop:disable Lint/UnusedMethodArgument
-      scope.order(c_t[:veteran].desc, c_t[:family_member].desc, c_t[:is_currently_youth].desc, Arel.sql("case when date_part('year', age(\"clients\".\"date_of_birth\")) BETWEEN 18 and 24 then 1 else 0 end desc"), c_t[:days_homeless].desc)
+      # NOTE: after Rails 6.1 upgrade, these could be written c_t[:veteran].desc.nulls_last
+      vets = Arel.sql(c_t[:veteran].desc.to_sql + ' NULLS LAST')
+      family = Arel.sql(c_t[:family_member].desc.to_sql + ' NULLS LAST')
+      is_youth = Arel.sql(c_t[:is_currently_youth].desc.to_sql + ' NULLS LAST')
+      age_youth = Arel.sql("case when date_part('year', age(\"clients\".\"date_of_birth\")) BETWEEN 18 and 24 then 1 else 0 end desc")
+      days = Arel.sql(c_t[:days_homeless].desc.to_sql + ' NULLS LAST')
+      scope.order(vets, family, is_youth, age_youth, days)
     end
 
     def self.client_prioritization_value_method
