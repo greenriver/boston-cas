dade3deacd317dad2329cfa05e846564819a568c
Gig Ashton <gig@greenriver.org>
2022-04-28 14:39:15 -0400

    Rename back to Match Group so it makes sense as column header

--- app/models/match_prioritization/match_group.rb
+++ app/models/match_prioritization/match_group.rb
@@ -7,7 +7,7 @@
 module MatchPrioritization
   class MatchGroup < Base
     def self.title
-      'Match Group, Entry Date, VI-SPDAT score'
+      'Match Group'
     end
 
     def self.prioritization_for_clients(scope, match_route:) # rubocop:disable Lint/UnusedMethodArgument
