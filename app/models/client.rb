15d10a43a1e61bc5292d2de914a49c10f9efc57e
Sandy Wise <sandy@greenriver.org>
2022-07-11 14:58:49 -0400

    Display match prioritization description in same order as prioritization

--- app/models/client.rb
+++ app/models/client.rb
@@ -350,10 +350,19 @@ class Client < ApplicationRecord
     tags.try(:[], tag_id.to_s)
   end
 
-  def days_homeless_populations(*)
+  def days_homeless_veterans_families_youth(*)
     {
       'Veteran' => yes_no(veteran),
       'Adult and Child' => yes_no(family_member),
+      'Youth' => yes_no(is_currently_youth || age&.between?(18, 24)),
+      'Days Homeless' => days_homeless,
+    }.map { |k, v| "#{k}: #{v}" }.join('; ')
+  end
+
+  def days_homeless_populations(*)
+    {
+      'Adult and Child' => yes_no(family_member),
+      'Veteran' => yes_no(veteran),
       'Youth' => yes_no(is_currently_youth || age&.between?(18, 24)),
       'Days Homeless' => days_homeless,
     }.map { |k, v| "#{k}: #{v}" }.join('; ')
