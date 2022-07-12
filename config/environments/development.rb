b7880f333dcf9b03ff83a2649a6a68ae89856dd7
Elliot Anders <elliot@greenriver.org>
2022-07-12 16:14:25 -0700

    Use non-JSON logs for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -89,6 +89,7 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
 
   config.log_level = ENV.fetch('LOG_LEVEL') { 'debug' }.to_sym
+  config.logger.formatter = ActiveSupport::Logger::SimpleFormatter.new
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
