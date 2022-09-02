29b3832d216e82fd5e51c1a27fd6f027706d19fc
Gig Ashton <gig@greenriver.org>
2022-09-02 12:15:25 -0400

    Secondary sort by veteran for match_group

--- app/models/match_prioritization/match_group.rb
+++ app/models/match_prioritization/match_group.rb
@@ -13,11 +13,12 @@ module MatchPrioritization
     def self.prioritization_for_clients(scope, match_route:) # rubocop:disable Lint/UnusedMethodArgument
       # NOTE: after Rails 6.1 upgrade, these could be written c_t[:veteran].desc.nulls_last
       match_group = Arel.sql(c_t[:match_group].asc.to_sql + ' NULLS LAST') # 1, 2, 3
+      veteran = Arel.sql(c_t[:veteran].desc.to_sql + ' NULLS LAST') # veteran prioritized over non-veteran
       chronic = Arel.sql(c_t[:chronic_homeless].desc.to_sql + ' NULLS LAST') # chronic prioritized over non-chronic
       entry_date = Arel.sql(c_t[:entry_date].asc.to_sql + ' NULLS LAST') # older prioritized over newer
       vispdat_score = Arel.sql(c_t[:vispdat_score].desc.to_sql + ' NULLS LAST') # higher prioritized over lover
 
-      scope.order(match_group, chronic, entry_date, vispdat_score)
+      scope.order(match_group, veteran, chronic, entry_date, vispdat_score)
     end
 
     def self.supporting_data_columns
