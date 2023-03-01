af9c7ce6903096a60ebb97a4ba8ea0ce10162ddb
Sandy Wise <sandy@greenriver.org>
2023-03-01 10:06:57 -0500

    Template for moving decline reasons into steps.

---
+++ app/models/concerns/match_decisions/default_dnd_staff_decline_reasons.rb
@@ -0,0 +1,21 @@
+###
+# Copyright 2016 - 2022 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/boston-cas/blob/production/LICENSE.md
+###
+
+module MatchDecisions
+  module DefaultDndStaffDeclineReasons
+    extend ActiveSupport::Concern
+
+    def step_decline_reasons(_contact)
+      @step_decline_reasons ||= [
+        "Client won't be eligible for services",
+        "Client won't be eligible for housing type",
+        "Client won't be eligible based on funding source",
+        'Client has another housing option',
+        'Other',
+      ]
+    end
+  end
+end
