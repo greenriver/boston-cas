1aa2d63e9bc5603a54f2ee7fda494df34759a98a
Sandy Wise <sandy@greenriver.org>
2023-03-02 09:12:36 -0500

    Update match route eight to use match reason concerns.

--- app/models/match_decisions/eight/eight_record_voucher_date.rb
+++ app/models/match_decisions/eight/eight_record_voucher_date.rb
@@ -91,8 +91,17 @@ module MatchDecisions::Eight
         contact.in?(match.send(contact_actor_type))
     end
 
-    private def decline_reason_scope(_contact)
-      MatchDecisionReasons::HousingSubsidyAdminDecline.available(route: match_route)
+    def step_decline_reasons(_contact)
+      @step_decline_reasons ||= [
+        'Immigration status',
+        'Ineligible for Housing Program',
+        'Self-resolved',
+        'Household did not respond after initial acceptance of match',
+        'Client refused offer',
+        'Client needs higher level of care',
+        'Unable to reach client after multiple attempts',
+        'Other',
+      ].freeze
     end
 
     class StatusCallbacks < StatusCallbacks
