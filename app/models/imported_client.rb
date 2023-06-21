e7459c06f70e0ff90f241c4bd79783416afdc352
Elliot Anders <elliot@greenriver.org>
2023-06-21 14:40:30 -0500

    Rubocop fixups

--- app/models/imported_client.rb
+++ app/models/imported_client.rb
@@ -17,19 +17,18 @@ class ImportedClient < NonHmisClient
   validates :email, presence: true
 
   # Allow same search rules as Client
-  scope :text_search, -> (text) do
+  scope :text_search, ->(text) do
     return none unless text.present?
+
     text.strip!
     sa = arel_table
-    numeric = /[\d-]+/.match(text).try(:[], 0) == text
     date = /\d\d\/\d\d\/\d\d\d\d/.match(text).try(:[], 0) == text
     social = /\d\d\d-\d\d-\d\d\d\d/.match(text).try(:[], 0) == text
     # Explicitly search for only last, first if there's a comma in the search
     if text.include?(',')
       last, first = text.split(',').map(&:strip)
-      if last.present?
-        where = search_last_name(last).or(search_alternate_name(last))
-      end
+      where = search_last_name(last).or(search_alternate_name(last)) if last.present?
+
       if last.present? && first.present?
         where = where.and(search_first_name(first)).or(search_alternate_name(first))
       elsif first.present?
@@ -38,10 +37,10 @@ class ImportedClient < NonHmisClient
       # Explicity search for "first last"
     elsif text.include?(' ')
       first, last = text.split(' ').map(&:strip)
-      where = search_first_name(first)
-          .and(search_last_name(last))
-          .or(search_alternate_name(first))
-          .or(search_alternate_name(last))
+      where = search_first_name(first).
+        and(search_last_name(last)).
+        or(search_alternate_name(first)).
+        or(search_alternate_name(last))
       # Explicitly search for a PersonalID
     elsif social
       where = sa[:ssn].eq(text.gsub('-', ''))
