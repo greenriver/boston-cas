653787ef2ba4038ef463d8500b9a396a4840a827
Dave G <149399758+dtgreiner@users.noreply.github.com>
2024-01-08 09:26:33 -0500

    Update missing rule text for housing barrier rule to correctly reference itself (#766)

--- app/models/rules/housing_barrier.rb
+++ app/models/rules/housing_barrier.rb
@@ -3,7 +3,7 @@ class Rules::HousingBarrier < Rule
     if Client.column_names.include?(:housing_barrier.to_s)
       scope.where(housing_barrier: requirement.positive)
     else
-      raise RuleDatabaseStructureMissing.new("clients.enrolled_in_th missing. Cannot check clients against #{self.class}.")
+      raise RuleDatabaseStructureMissing.new("clients.housing_barrier missing. Cannot check clients against #{self.class}.")
     end
   end
 end
