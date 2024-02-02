0828d6aeb31849e2dc399e40141faf6854c73dd3
Elliot <elliot@greenriver.org>
2024-02-02 15:06:51 -0500

    Copyright logic from the warehouse; update existing files with 2023 version; Update everything to 2024 (#771)

--- app/models/rules/service_need.rb
+++ app/models/rules/service_need.rb
@@ -1,3 +1,9 @@
+###
+# Copyright 2016 - 2024 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/boston-cas/blob/production/LICENSE.md
+###
+
 class Rules::ServiceNeed < Rule
   def clients_that_fit(scope, requirement, opportunity)
     if Client.column_names.include?(:service_need.to_s)
---
+++ lib/util/code.rb
@@ -0,0 +1,18 @@
+###
+# Copyright 2016 - 2024 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/boston-cas/blob/production/LICENSE.md
+###
+
+class Code
+  def self.copywright_header
+    <<~COPYRIGHT
+      ###
+      # Copyright 2016 - 2024 Green River Data Analysis, LLC
+      #
+      # License detail: https://github.com/greenriver/boston-cas/blob/production/LICENSE.md
+      ###
+
+    COPYRIGHT
+  end
+end
