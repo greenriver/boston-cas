f8a5f1a7eaf75fd6914ead0d60acb32b6e7cd24f
Todd Blackman <tdblackman@gmail.com>
2024-02-20 16:27:41 -0500

    Something weird is going on around bundle exec but I can't find the source of the double bundle exec-ing

--- bin/deploy_tasks.sh
+++ bin/deploy_tasks.sh
@@ -6,49 +6,49 @@ echo Migrating with individual rake tasks
 
 echo Migrating app database
 T1=`date +%s`
-bundle exec rake db:migrate
+rake db:migrate
 T2=`date +%s`
 echo "...rake db:migrate took $(expr $T2 - $T1) seconds"
 
 echo 'Rules seeding'
 T1=`date +%s`
-bundle exec rake cas_seeds:create_rules
+rake cas_seeds:create_rules
 T2=`date +%s`
 echo "...rake cas_seeds:create_rules took $(expr $T2 - $T1) seconds"
 
 echo 'Match decision reasons seeding'
 T1=`date +%s`
-bundle exec rake cas_seeds:create_match_decision_reasons
+rake cas_seeds:create_match_decision_reasons
 T2=`date +%s`
 echo "...rake cas_seeds:create_match_decision_reasons took $(expr $T2 - $T1) seconds"
 
 echo 'Ensure all match routes exist'
 T1=`date +%s`
-bundle exec rake cas_seeds:ensure_all_match_routes_exist
+rake cas_seeds:ensure_all_match_routes_exist
 T2=`date +%s`
 echo "...rake cas_seeds:ensure_all_match_routes_exist took $(expr $T2 - $T1) seconds"
 
 echo 'Ensure all match prioritization schemes exist'
 T1=`date +%s`
-bundle exec rake cas_seeds:ensure_all_match_prioritization_schemes_exist
+rake cas_seeds:ensure_all_match_prioritization_schemes_exist
 T2=`date +%s`
 echo "...rake cas_seeds:ensure_all_match_prioritization_schemes_exist took $(expr $T2 - $T1) seconds"
 
 echo 'Stalled reasons seeding'
 T1=`date +%s`
-bundle exec rake cas_seeds:stalled_reasons
+rake cas_seeds:stalled_reasons
 T2=`date +%s`
 echo "...rake cas_seeds:stalled_reasons took $(expr $T2 - $T1) seconds"
 
 echo 'Mitigation reasons seeding'
 T1=`date +%s`
-bundle exec rake cas_seeds:create_mitigation_reasons
+rake cas_seeds:create_mitigation_reasons
 T2=`date +%s`
 echo "...rake cas_seeds:create_mitigation_reasons took $(expr $T2 - $T1) seconds"
 
 echo 'General seeding'
 T1=`date +%s`
-bundle exec rake db:seed
+rake db:seed
 T2=`date +%s`
 echo "...rake db:seed took $(expr $T2 - $T1) seconds"
 
@@ -60,5 +60,5 @@ echo "..../cron_installer.rb took $(expr $T2 - $T1) seconds"
 
 # keep this always at the end of this file
 echo making interface aware this script completed
-bundle exec rake deploy:mark_deployment_id
+rake deploy:mark_deployment_id
 echo ---DONE---
