227a15dd5ae81249159946d78325e10f4ef9ede6
Dave G <dgreiner@greenriver.org>
2024-02-27 10:02:18 -0500

    Add rule for matching exact number of bedrooms

---
+++ app/models/rules/bedroom_exact.rb
@@ -0,0 +1,37 @@
+###
+# Copyright 2016 - 2024 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/boston-cas/blob/production/LICENSE.md
+###
+
+class Rules::BedroomExact < Rule
+  def variable_requirement?
+    true
+  end
+
+  def available_number_of_bedrooms
+    [
+      [1, 'One'],
+      [2, 'Two'],
+      [3, 'Three'],
+      [4, 'Four'],
+      [5, 'Five'],
+    ]
+  end
+
+  def display_for_variable value
+    available_number_of_bedrooms.to_h.try(:[], value.to_i) || value
+  end
+
+  def clients_that_fit(scope, requirement, opportunity)
+    if Client.column_names.include?(:required_number_of_bedrooms.to_s)
+      if requirement.positive
+        scope.where(c_t[:required_number_of_bedrooms].eq(requirement.variable))
+      else
+        scope.where(c_t[:required_number_of_bedrooms].not_eq(requirement.variable))
+      end
+    else
+      raise RuleDatabaseStructureMissing.new("clients.required_number_of_bedrooms missing. Cannot check clients against #{self.class}.")
+    end
+  end
+end
