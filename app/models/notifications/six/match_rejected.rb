845c4e78cef3bad1004a6f5d782f3d16a35f4b18
Sandy Wise <33072305+awisema@users.noreply.github.com>
2024-05-14 16:13:40 -0400

    Add missing route 6 notifications (#810)

---
+++ app/models/notifications/six/match_rejected.rb
@@ -0,0 +1,10 @@
+###
+# Copyright 2016 - 2024 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/boston-cas/blob/production/LICENSE.md
+###
+
+module Notifications::Six
+  class MatchRejected < Notifications::MatchRejected
+  end
+end
