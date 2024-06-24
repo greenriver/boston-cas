9d8cd98ce5f201a02fd18953647b2a5208639e0f
Dave G <dgreiner@greenriver.org>
2024-06-24 08:58:38 -0400

    more rubocop fixes

--- db/migrate/20240621125508_match_route_eleven_translations.rb
+++ db/migrate/20240621125508_match_route_eleven_translations.rb
@@ -6,22 +6,23 @@ class MatchRouteElevenTranslations < ActiveRecord::Migration[7.0]
         text: v,
       )
     end
-  end 
+  end
 
   def down
     Translation.where(key: translations_eleven.keys).destroy_all
   end
 
   private
-    def translations_eleven = {
-      'Match Route Eleven' => 'Match Route Eleven',
-      'SSP Eleven' => 'SSP',
-      'Shelter Agency Eleven' => 'Shelter Agency',
-      'HSA Eleven' => 'HSA',
-      'Housing Search Provider Eleven' => 'Housing Search Provider',
-      'Housing Subsidy Administrator Eleven' => 'Housing Subsidy Administrator',
-      'Stabilization Service Provider Eleven' => 'Stabilization Services Provider',
-      'Stabilization Services Provider Eleven' => 'Stabilization Services Provider',
-      'CoC Eleven' => 'DND',
-    }
+
+  def translations_eleven = {
+    'Match Route Eleven' => 'Match Route Eleven',
+    'SSP Eleven' => 'SSP',
+    'Shelter Agency Eleven' => 'Shelter Agency',
+    'HSA Eleven' => 'HSA',
+    'Housing Search Provider Eleven' => 'Housing Search Provider',
+    'Housing Subsidy Administrator Eleven' => 'Housing Subsidy Administrator',
+    'Stabilization Service Provider Eleven' => 'Stabilization Services Provider',
+    'Stabilization Services Provider Eleven' => 'Stabilization Services Provider',
+    'CoC Eleven' => 'DND',
+  }
 end
