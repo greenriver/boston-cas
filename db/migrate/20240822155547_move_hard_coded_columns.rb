25ba2e3b9c63b21ccf072cfdaa8fa9dd7ea38709
Elliot Anders <elliot@greenriver.org>
2024-08-22 12:23:01 -0400

    Account for various configuration options

--- db/migrate/20240822155547_move_hard_coded_columns.rb
+++ db/migrate/20240822155547_move_hard_coded_columns.rb
@@ -4,10 +4,11 @@ class MoveHardCodedColumns < ActiveRecord::Migration[7.0]
       :assessment_type_description,
       :required_number_of_bedrooms_or_sro_ok,
     ]
+    existing_columns << :required_minimum_occupancy unless Config.get(:identified_client_assessment).include?('Pathways')
     MatchRoutes::Base.find_each do |r|
       previous_columns = r.prioritized_client_columns || []
       visible_columns = existing_columns + previous_columns
-      r.update!(prioritized_client_columns: visible_columns)
+      r.update!(prioritized_client_columns: visible_columns.uniq)
     end
   end
 end
