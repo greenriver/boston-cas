ba336cb3a4f98e42720f08e328e0fbbbec5d978b
Dave G <dgreiner@greenriver.org>
2024-09-17 08:36:14 -0400

    Update migration to set contacts that are the only contact of that type on a match as the primary contact

--- db/migrate/20240827172144_add_contact_order_to_contacts.rb
+++ db/migrate/20240827172144_add_contact_order_to_contacts.rb
@@ -1,5 +1,20 @@
 class AddContactOrderToContacts < ActiveRecord::Migration[7.0]
   def change
     add_column :client_opportunity_match_contacts, :contact_order, :integer
+    
+    # Set contact_order to 1 for all single contact types on a match
+    [
+      :dnd_staff,
+      :housing_subsidy_admin,
+      :client,
+      :housing_search_worker,
+      :shelter_agency,
+      :ssp,
+      :hsp,
+      :do,
+    ].each do |type|
+      match_ids = ClientOpportunityMatchContact.select(:match_id).where(type => true).group(:match_id).having('count(*) = 1').pluck(:match_id)
+      ClientOpportunityMatchContact.where(match_id: match_ids, type => type).update_all(contact_order: 1)
+    end
   end
 end
