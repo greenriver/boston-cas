694c4688cb1d7e38ba5819779ff4098c6538a0f3
Todd Blackman <tdblackman@gmail.com>
2024-10-02 09:03:43 -0400

    rubocop fixes

--- bin/database-ready.rb
+++ bin/database-ready.rb
@@ -56,7 +56,7 @@ class DbTester
         downs += 1 if status == 'down'
       end
 
-      return if ups > 3 && downs == 0
+      return if ups > 3 && downs.zero?
 
       puts "Less than 3 migrations up or a migration has not run yet. Trying again in #{SLEEP_TIME_SEC} seconds."
 
