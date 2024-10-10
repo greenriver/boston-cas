ae5a3d6bdf56de95628dba4daf474948e530fade
Dave G <dgreiner@greenriver.org>
2024-10-10 15:58:03 -0400

    rubocop

--- db/migrate/20241010194153_destroy_route_eleven_matches.rb
+++ db/migrate/20241010194153_destroy_route_eleven_matches.rb
@@ -1,8 +1,5 @@
 class DestroyRouteElevenMatches < ActiveRecord::Migration[7.0]
   def change
-    unless Rails.env.production? 
-      matches = ClientOpportunityMatch.where(match_route: MatchRoutes::Eleven.ids)
-      ClientOpportunityMatch.destroy(matches.ids)
-    end
+    ClientOpportunityMatch.joins(:match_route).where(match_routes: {type: 'MatchRoutes::Eleven'}).destroy_all unless Rails.env.production?
   end
 end
