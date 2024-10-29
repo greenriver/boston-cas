8e6b1f0afcf6665ff5116278618f9ca8145d7de1
Dave G <dgreiner@greenriver.org>
2024-10-29 13:23:01 -0400

    clean up unused match decision reasons

--- app/models/match_decision_reasons/administrative_cancel.rb
+++ app/models/match_decision_reasons/administrative_cancel.rb
@@ -6,36 +6,6 @@
 
 module MatchDecisionReasons
   class AdministrativeCancel < Base
-    def self.available(include_other: true, route: nil) # rubocop:disable Lint/UnusedMethodArgument
-      other = MatchDecisionReasons::Base.other.first
-      # none = OpenStruct.new(name: 'None', id: nil)
-      av = active.to_a
-      # av << none
-      if route.present?
-        av.reject! { |reason| route.removed_admin_reasons.include?(reason.name) } if route.removed_admin_reasons.any?
-        av += limited.where(name: route.additional_admin_reasons).to_a if route.additional_admin_reasons.any?
-      end
-      av << other
-
-      av
-    end
-
-    def self.available_for_provider_only(include_other: true, route: nil) # rubocop:disable Lint/UnusedMethodArgument
-      other = MatchDecisionReasons::Base.other.first
-      # none = OpenStruct.new(name: 'None', id: nil)
-      av = active.where(name: provider_only_options).to_a
-      av << other if include_other
-
-      av
-    end
-
-    def self.provider_only_options
-      [
-        'Vacancy should not have been entered',
-        'Vacancy filled by other client',
-      ]
-    end
-
     def title
       Translation.translate('Administrative Cancelation')
     end
