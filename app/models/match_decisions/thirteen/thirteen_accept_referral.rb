2c0512e863ed62613e45eb45678bacc526bf953f
Elliot Anders <elliot@greenriver.org>
2025-01-16 09:43:56 -0500

    Include reason in label when declining matches on route 13

--- app/models/match_decisions/thirteen/thirteen_accept_referral.rb
+++ app/models/match_decisions/thirteen/thirteen_accept_referral.rb
@@ -58,7 +58,7 @@ module MatchDecisions::Thirteen
       when :pending then "#{Translation.translate('HSA Thirteen')} assigned match"
       when :accepted then "Referral Accepted by #{Translation.translate('HSA Thirteen')}."
       when :canceled then canceled_status_label
-      when :declined then 'Match Declined'
+      when :declined then "Match Declined.  Reason: #{decline_reason_name}"
       when :back then backup_status_label
       end
     end
--- app/models/match_decisions/thirteen/thirteen_hsa_review.rb
+++ app/models/match_decisions/thirteen/thirteen_hsa_review.rb
@@ -52,7 +52,7 @@ module MatchDecisions::Thirteen
       when :pending then "#{Translation.translate('HSA Thirteen')} assigned match"
       when :accepted then "Match Reeviewed by #{Translation.translate('HSA Thirteen')}."
       when :canceled then canceled_status_label
-      when :declined then 'Match Declined'
+      when :declined then "Match Declined.  Reason: #{decline_reason_name}"
       when :back then backup_status_label
       end
     end
