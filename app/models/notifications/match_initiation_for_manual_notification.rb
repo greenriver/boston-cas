ce70a34beca32db39283e7640446f4671ece8562
Elliot <elliot@greenriver.org>
2025-01-22 16:37:43 -0500

    When the initial contact can't see the possible matches, don't send a link (#905)

--- app/models/notifications/match_initiation_for_manual_notification.rb
+++ app/models/notifications/match_initiation_for_manual_notification.rb
@@ -6,15 +6,19 @@
 
 module Notifications
   class MatchInitiationForManualNotification < Notifications::Base
-
     def self.create_for_match! match
       match.send(match.match_route.initial_contacts_for_match).each do |contact|
+        # this notification includes a link to the un-started matches, don't send anything
+        # if the user can't access the link
+        user = contact.user
+        next unless user&.can_see_alternate_matches? || user&.can_see_all_alternate_matches?
+
         create! match: match, recipient: contact
       end
     end
 
     def event_label
-      "Initial contacts notified of manual match"
+      'Initial contacts notified of manual match'
     end
   end
 end
