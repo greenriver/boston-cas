e5be3247b98954e8d49a8b952dc5466fc4d7a1fa
Elliot <elliot@greenriver.org>
2025-04-08 12:50:42 -0400

    Setup stall contact relationships for various route 13 decisions (#930)
    * Setup stall contact relationships for various route 13 decisions
    
    * SSP notifications
    
    * Remove unnecessary method

--- app/models/match_decisions/thirteen/thirteen_hsa_review.rb
+++ app/models/match_decisions/thirteen/thirteen_hsa_review.rb
@@ -4,6 +4,8 @@
 # License detail: https://github.com/greenriver/boston-cas/blob/production/LICENSE.md
 ###
 
+# frozen_string_literal: true
+
 module MatchDecisions::Thirteen
   class ThirteenHsaReview < Base
     include MatchDecisions::AcceptsDeclineReason
@@ -34,6 +36,7 @@ module MatchDecisions::Thirteen
         m << Notifications::Thirteen::ThirteenHsaReviewShelterAgency
         m << Notifications::Thirteen::ThirteenHsaReviewHsa
         m << Notifications::Thirteen::ThirteenHsaReviewDndStaff
+        m << Notifications::Thirteen::ThirteenHsaReviewSsp
       end
     end
 
@@ -67,6 +70,15 @@ module MatchDecisions::Thirteen
       true
     end
 
+    def stalled_contact_types
+      @stalled_contact_types ||= [
+        :shelter_agency_contacts,
+        :housing_subsidy_admin_contacts,
+        :dnd_staff_contacts,
+        :ssp_contacts,
+        :do_contacts,
+      ]
+    end
     private def ensure_required_contacts_present_on_accept
       missing_contacts = []
       missing_contacts << "a #{Translation.translate('Shelter Agency Thirteen')} Contact" if save_will_accept? && match.shelter_agency_contacts.none?
