15a553720ed6c6144c699a58d826084cdbe3f5c0
Elliot <elliot@greenriver.org>
2025-04-08 12:34:01 -0400

    Update app/models/warehouse/analytics.rb
    Co-authored-by: Theron Toomey <ttoomey@greenriver.org>

--- app/models/warehouse/analytics.rb
+++ app/models/warehouse/analytics.rb
@@ -5,7 +5,10 @@
 ###
 
 # frozen_string_literal: true
-
+# The Analytics module supports export of CAS to our analytics store. These tables are consumed view views in the warehouse analytics schema
+#
+# Data Flow:
+# CAS::Analytics --> CAS db connection --> analytics views (warehouse)  --> DBT (external) 
 module Warehouse::Analytics
   def self.table_prefix = 'cas_analytics_'
 end
