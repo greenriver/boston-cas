3da4983a615fa77c442f1d923f2f4074b3a4ef76
Theron Toomey <ttoomey@greenriver.org>
2025-04-24 04:39:58 +0000

    rspec

--- app/models/warehouse/base.rb
+++ app/models/warehouse/base.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 ###
 # Copyright 2016 - 2025 Green River Data Analysis, LLC
 #
@@ -9,13 +11,17 @@ module Warehouse
     self.abstract_class = true
     connects_to(
       database: {
-      writing: "#{Rails.env}_warehouse".parameterize.underscore.to_sym,
-      reading: "#{Rails.env}_warehouse".parameterize.underscore.to_sym
-      }
+        writing: "#{Rails.env}_warehouse".parameterize.underscore.to_sym,
+        reading: "#{Rails.env}_warehouse".parameterize.underscore.to_sym,
+      },
     )
 
     def self.enabled?
-      @enabled ||= Warehouse::Base.connection.active? && Warehouse::Base.connection.data_source_exists?('cohorts') rescue false
+      @enabled ||= begin
+                     Warehouse::Base.connection.active? && Warehouse::Base.connection.data_source_exists?('cohorts')
+                   rescue StandardError
+                     false
+                   end
     end
   end
 end
--- spec/models/client_notes_spec.rb
+++ spec/models/client_notes_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe ClientNote, type: :model do
