1dc1b4b79f99030a652e529bbac20dafa16f6638
Theron Toomey <ttoomey@greenriver.org>
2025-04-24 22:11:34 +0000

    get migrations to run

--- db/migrate/20170428201839_add_active_to_match_decision_reasons.rb
+++ db/migrate/20170428201839_add_active_to_match_decision_reasons.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class AddActiveToMatchDecisionReasons < ActiveRecord::Migration[4.2]
   def change
     add_column :match_decision_reasons, :active, :boolean, default: true, null: false
-    MatchDecisionReasons::AdministrativeCancel.
-      where(name: ['7 days expired', '14 days expired']).
-      update_all(active: false)
+    # MatchDecisionReasons::AdministrativeCancel.
+    #   where(name: ['7 days expired', '14 days expired']).
+    #   update_all(active: false)
   end
 end
