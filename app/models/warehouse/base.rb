b7e2e02bfc8253174ecf67e00f458ba588850ee5
Theron Toomey <ttoomey@greenriver.org>
2025-04-25 00:05:49 +0000

    config

--- app/models/warehouse/base.rb
+++ app/models/warehouse/base.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 ###
 # Copyright 2016 - 2025 Green River Data Analysis, LLC
 #
@@ -9,14 +7,10 @@
 module Warehouse
   class Base < ActiveRecord::Base
     self.abstract_class = true
-    connects_to(database: { writing: "#{Rails.env}_warehouse".parameterize.underscore.to_sym, reading: "#{Rails.env}_warehouse".parameterize.underscore.to_sym })
+    establish_connection "#{Rails.env}_warehouse".parameterize.underscore.to_sym
 
     def self.enabled?
-      @enabled ||= begin
-                     Warehouse::Base.connection.active? && Warehouse::Base.connection.data_source_exists?('cohorts')
-                   rescue StandardError
-                     false
-                   end
+      @enabled ||= Warehouse::Base.connection.active? && Warehouse::Base.connection.data_source_exists?('cohorts') rescue false
     end
   end
 end
