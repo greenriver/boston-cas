94ef9f86c2de0fc0709af143716ca2137090410b
Theron Toomey <ttoomey@greenriver.org>
2025-04-25 03:25:42 +0000

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   config.i18n.fallbacks = true
 
   # Don't log any deprecations.
-  config.active_support.report_deprecations = false
+  config.active_support.report_deprecations = true
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
