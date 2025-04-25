135ceb039b5944e8c55b65df4bd5cad44101bec3
Theron Toomey <ttoomey@greenriver.org>
2025-04-25 16:40:26 +0000

    rubocop

--- config/deploy/docker/lib/asset_compiler.rb
+++ config/deploy/docker/lib/asset_compiler.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'active_support/core_ext/object/blank'
 require_relative 'aws_sdk_helpers'
 require 'dotenv'
@@ -5,8 +7,8 @@ require 'dotenv'
 class AssetCompiler
   include AwsSdkHelpers::Helpers
 
-  THEME_ASSETS_BUCKET = 'openpath-ecs-assets'.freeze
-  COMPILED_ASSETS_BUCKET = 'openpath-precompiled-assets'.freeze
+  THEME_ASSETS_BUCKET = 'openpath-ecs-assets'
+  COMPILED_ASSETS_BUCKET = 'openpath-precompiled-assets'
 
   def initialize(target_group_name:, secret_arn: nil)
     @target_group_name = target_group_name
