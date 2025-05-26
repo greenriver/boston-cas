67745b94dc40389c39004e46c2b28bd3da9cace8
Theron Toomey <ttoomey@greenriver.org>
2025-05-26 22:56:15 +0000

    fix

--- spec/factories/deidentified_clients.rb
+++ spec/factories/deidentified_clients.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     client_identifier { Faker::Number.number(digits: 4) }
     first_name { "Anonymous - #{client_identifier}" }
     last_name  { "Anonymous - #{client_identifier}" }
+    identified { false }
   end
 end
