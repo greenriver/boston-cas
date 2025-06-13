55de611594749b35d7cd91a90db25f6f888a1968
Elliot <elliot@greenriver.org>
2025-06-13 16:45:31 -0400

    Allow updating of expiration date regardless of step, we control access to the form by step (#959)

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -4,6 +4,8 @@
 # License detail: https://github.com/greenriver/boston-cas/blob/production/LICENSE.md
 ###
 
+# frozen_string_literal: true
+
 class Opportunity < ApplicationRecord
   acts_as_paranoid
   has_paper_trail
@@ -166,11 +168,11 @@ class Opportunity < ApplicationRecord
   # Return prioritized clients who match this opportunity
   def matching_clients(client_scope = Client.available_for_matching(match_route))
     requirements_with_inherited.each do |requirement|
-      client_scope = client_scope.merge(requirement.clients_that_fit(client_scope, self), rewhere: true)
+      client_scope = client_scope.merge(requirement.clients_that_fit(client_scope, self))
     end
     client_scope = add_unit_attributes_filter(client_scope)
     client_scope = client_scope.not_rejected_for(id)
-    client_scope.merge(Client.prioritized(active_prioritization_scheme, client_scope), rewhere: true)
+    client_scope.merge(Client.prioritized(active_prioritization_scheme, client_scope))
   end
 
   def add_unit_attributes_filter(client_scope)
