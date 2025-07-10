20ea6a55d1482eddf9e8ac553b0ed00af1334044
Elliot <elliot@greenriver.org>
2025-07-10 15:35:08 -0400

    Various fixes for incorrect associations and display (#967)

--- app/models/concerns/match_archive.rb
+++ app/models/concerns/match_archive.rb
@@ -4,6 +4,8 @@
 # License detail: https://github.com/greenriver/boston-cas/blob/production/LICENSE.md
 ###
 
+# frozen_string_literal: true
+
 # Methods used to save the state of the universe during match creation
 module MatchArchive
   # This module serves as a record of what is needed to get a model to work with the requirement manager
@@ -11,19 +13,18 @@ module MatchArchive
   extend ActiveSupport::Concern
 
   def prepare_for_archive
-    attributes = if self.is_a?(OpportunityDetails::Base)
+    attributes = if is_a?(OpportunityDetails::Base)
       opportunity.attributes
     else
       self.attributes
     end
     return unless attributes.present?
-    if respond_to?(:services) && services.any?
-      attributes[:services] = services.map(&:prepare_for_archive)
-    end
+
+    attributes[:services] = services.map(&:prepare_for_archive) if respond_to?(:services) && services.any?
     if respond_to?(:requirements) && requirements.any?
       attributes[:requirements] = requirements.map(&:prepare_for_archive)
-      attributes[:rules] = requirements.map{|m| {type: m.rule.type, id: m.id}}
+      attributes[:rules] = requirements.map { |m| { type: m.rule&.type, id: m.id } }
     end
-    attributes.reject{|k,v| ['deleted_at', 'created_at', 'updated_at'].include? k}
+    attributes.reject { |k, _v| ['deleted_at', 'created_at', 'updated_at'].include?(k) }
   end
 end
