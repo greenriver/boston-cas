b43c71737fb4d4414e2825d8724cd2c0d905db2e
Elliot Anders <elliot@greenriver.org>
2025-08-25 12:50:53 -0400

    Debugging CI

--- spec/controllers/closed_matches_controller_search_spec.rb
+++ spec/controllers/closed_matches_controller_search_spec.rb
@@ -14,12 +14,14 @@ RSpec.describe ClosedMatchesController, type: :controller do
   let!(:voucher2) { create(:voucher, sub_program: create(:sub_program, program: program)) }
   let!(:opportunity1) { create(:opportunity, voucher: voucher1) }
   let!(:opportunity2) { create(:opportunity, voucher: voucher2) }
-  let!(:closed_match_1) { create(:successful_client_opportunity_match, client: client1, opportunity: opportunity1) }
-  let!(:closed_match_2) { create(:unsuccessful_client_opportunity_match, client: client2, opportunity: opportunity2) }
+
+  # Create a route first, then use it in the matches
+  let!(:match_route) { create(:default_route) }
+  let!(:closed_match_1) { create(:client_opportunity_match, client: client1, opportunity: opportunity1, match_route: match_route, closed: true, active: false) }
+  let!(:closed_match_2) { create(:client_opportunity_match, client: client2, opportunity: opportunity2, match_route: match_route, closed: true, active: false) }
 
   before do
     allow_any_instance_of(ClosedMatchesController).to receive(:setup_notifier)
-    authenticate admin
     admin.roles << admin_role
     allow(MatchRoutes::Base).to receive(:filterable_routes).and_return({ 'Default' => 'MatchRoutes::Default' })
   end
