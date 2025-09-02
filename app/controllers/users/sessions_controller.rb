5852217b2071b5aa447b7d9db8fb4c93976ad3ef
Elliot Anders <elliot@greenriver.org>
2025-09-02 14:50:19 -0400

    Rails 7.1+ raises when a controller doesn't implment a requested action

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,14 +4,19 @@
 # License detail: https://github.com/greenriver/boston-cas/blob/production/LICENSE.md
 ###
 
+# frozen_string_literal: true
+
 class Users::SessionsController < Devise::SessionsController
-  #configure auto_session_timeout
+  before_action :compose_activity, except: [:active]
+  after_action :log_activity, except: [:active]
+
+  # configure auto_session_timeout
   def active
     render_session_status
   end
 
   def timeout
-    flash[:notice] = "Your session expired; you have been logged out."
+    flash[:notice] = 'Your session expired; you have been logged out.'
     redirect_to root_path
   end
 end
