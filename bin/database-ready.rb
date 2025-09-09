00e51d90d277e4c90bc361aa0ccaf35c20acb031
Todd Blackman <tdblackman@gmail.com>
2025-09-09 11:01:36 -0400

    self QA

--- bin/database-ready.rb
+++ bin/database-ready.rb
@@ -116,6 +116,3 @@ end
 
 # rake db:migrate:status:primary works sometimes? I'm confused.
 DbTester.new(ApplicationRecord).run!
-DbTester.new(GrdaWarehouseBase).run!
-DbTester.new(ReportingBase).run!
-DbTester.new(HealthBase).run!
