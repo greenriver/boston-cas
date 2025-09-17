21202ae8ce676b247f603b819f3ec0c16509fcdb
Elliot Anders <elliot@greenriver.org>
2025-09-17 11:57:21 -0400

    Freeze strings

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -4,6 +4,8 @@
 # License detail: https://github.com/greenriver/boston-cas/blob/production/LICENSE.md
 ###
 
+# frozen_string_literal: true
+
 class UnitsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_can_view_units!
