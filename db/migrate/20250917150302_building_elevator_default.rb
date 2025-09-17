43e4e3b44433f147453a106d1ce11d8da9d88d2b
Elliot Anders <elliot@greenriver.org>
2025-09-17 11:29:56 -0400

    Comment

--- db/migrate/20250917150302_building_elevator_default.rb
+++ db/migrate/20250917150302_building_elevator_default.rb
@@ -1,3 +1,11 @@
+###
+# Copyright 2016 - 2025 Green River Data Analysis, LLC
+#
+# License detail: https://github.com/greenriver/boston-cas/blob/production/LICENSE.md
+###
+
+# frozen_string_literal: true
+
 class BuildingElevatorDefault < ActiveRecord::Migration[7.2]
   def change
     add_column :buildings, :elevator_accessible_default, :boolean, default: false
