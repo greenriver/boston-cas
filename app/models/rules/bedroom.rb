db13e5d1278f7de68e8c0de10f087e577a0fe848
Elliot Anders <elliot@greenriver.org>
2025-12-25 14:57:26 -0500

    Fix type, clarify second scenario

--- app/models/rules/bedroom.rb
+++ app/models/rules/bedroom.rb
@@ -20,7 +20,7 @@ class Rules::Bedroom < Rule
       ### Note
       If this voucher is attached to a 2 bedroom unit, to match a client who requires 2 or more bedrooms, choose "Can't have minimum number of bedrooms" with "One" selected as the number of bedrooms.
 
-      For a 3 bedroom unit, choose"Can't have minimum number of bedrooms" with "Two".
+      For a 3 bedroom unit, choose "Can't have minimum number of bedrooms" with "Two" selected as the number of bedrooms.
     NOTE
     Translation.translate(note)
   end
