9cb9778bb08047b78900b211a5e8a200c3ffaa7a
Elliot Anders <elliot@greenriver.org>
2026-02-13 09:16:52 -0500

    Enable papertrail for match routes

--- app/models/match_routes/base.rb
+++ app/models/match_routes/base.rb
@@ -12,6 +12,7 @@ module MatchRoutes
     serialize :prioritized_client_columns, type: Array
     serialize :routes_parked_on_active_match, type: Array
     serialize :routes_parked_on_successful_match, type: Array
+    has_paper_trail
 
     belongs_to :match_prioritization, class_name: 'MatchPrioritization::Base', foreign_key: :match_prioritization_id, primary_key: :id
     belongs_to :tag if column_names.include?('tag_id')
