9824068cd0e2ac300eae4a4ad4ed49daa331a016
Dave G <149399758+dtgreiner@users.noreply.github.com>
2026-03-04 10:12:26 -0500

    Update app/assets/javascripts/controllers/history_controller.js
    Co-authored-by: Elliot <elliot@greenriver.org>

--- app/assets/javascripts/controllers/history_controller.js
+++ app/assets/javascripts/controllers/history_controller.js
@@ -39,7 +39,7 @@
 
     initSelects() {
       if (typeof jQuery === 'undefined') return
-      if (this.hasCollapseSelectTarget) this.initSingleSelect(this.collapseSelectTarget, true,)
+      if (this.hasCollapseSelectTarget) this.initSingleSelect(this.collapseSelectTarget, true)
       if (this.hasEventSelectTarget) this.initSingleSelect(this.eventSelectTarget, true)
       if (this.hasContactSelectTarget) {
         const $el = jQuery(this.contactSelectTarget)
