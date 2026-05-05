942a722b2e1e0835b244ca46995fb606ed7240c0
Elliot Anders <elliot@greenriver.org>
2026-05-05 10:03:59 -0400

    No longer need to sync assets during deploy

--- bin/sync_app_assets.rb
+++ bin/sync_app_assets.rb
@@ -50,11 +50,13 @@ begin
       next
     end
 
-    resp = client.get_object({
-                               bucket: bucket,
-                               key: key,
-                               response_target: target,
-                             })
+    resp = client.get_object(
+      {
+        bucket: bucket,
+        key: key,
+        response_target: target,
+      },
+    )
   end
 rescue Aws::S3::Errors::NoSuchBucket
   puts "[#{__FILE__}] Cannot find the bucket: #{bucket}"
