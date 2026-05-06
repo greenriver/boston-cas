2ffd49b8b7c960726baa1a57239a71ace95fddf2
Elliot Anders <elliot@greenriver.org>
2026-05-06 13:21:45 -0400

    Revert active storage draw_routes change, we'll proxy files

--- config/application.rb
+++ config/application.rb
@@ -119,6 +119,7 @@ module BostonCa
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.active_storage.draw_routes = true
+    # Disable Active Storage routes
+    config.active_storage.draw_routes = false
   end
 end
