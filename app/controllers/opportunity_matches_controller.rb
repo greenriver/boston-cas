6b89b94b6abb16e234bc58901d9bb3eb1caa1925
Elliot Anders <elliot@greenriver.org>
2026-06-16 09:59:33 -0400

    Limit access to starting matches when the voucher isn't active

--- app/controllers/opportunity_matches_controller.rb
+++ app/controllers/opportunity_matches_controller.rb
@@ -122,10 +122,12 @@ class OpportunityMatchesController < ApplicationController
   end
 
   # You can activate a match if all of the following are true:
-  # 1. the match hasn't succeeded
-  # 2. you have can edit all clients or this opportunity is editable by you
-  # 3. you've been given the can activate matches permission
+  # 1. the opportunity's voucher is available
+  # 2. the match hasn't succeeded
+  # 3. you have can edit all clients or this opportunity is editable by you
+  # 4. you've been given the can activate matches permission
   def can_activate_match?
+    return false unless @opportunity.voucher&.available?
     return false if @opportunity.successful_match
     return false unless current_user.can_edit_all_clients? || @opportunity.editable_by?(current_user)
 
