56d0e05c69743ca91b4f477fd16768f14f6bcd14
Elliot Anders <elliot@greenriver.org>
2026-06-26 12:54:17 -0400

    Housing attributes and amenities

--- app/models/housing_attribute.rb
+++ app/models/housing_attribute.rb
@@ -4,23 +4,32 @@
 # License detail: https://github.com/greenriver/boston-cas/blob/stable/LICENSE.md
 ###
 
+# frozen_string_literal: true
+
 class HousingAttribute < ApplicationRecord
   acts_as_paranoid
   has_paper_trail
 
   belongs_to :housingable, polymorphic: true
 
-  validates_presence_of :name
-  validates_presence_of :value
+  scope :with_value, -> { where(include_value: true) }
+  scope :without_value, -> { where(include_value: false) }
 
-  def existing_attributes
-    HousingAttribute.pluck(:name).uniq.sort
+  validates_presence_of :name
+  validates :value, presence: true, if: :include_value?
+
+  def self.existing_attributes
+    with_value.pluck(:name).uniq.sort
+  end
+
+  def self.existing_amenities
+    without_value.pluck(:name).uniq.sort
   end
 
   def existing_values(for_attribute: nil)
     for_attribute ||= name
     return [] if for_attribute.blank?
 
-    HousingAttribute.where(name: for_attribute).pluck(:value).uniq.sort
+    self.class.with_value.where(name: for_attribute).pluck(:value).uniq.sort
   end
 end
