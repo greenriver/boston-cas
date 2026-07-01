c95ac2ce7f61db5c24b66be632951a31b483eedd
Elliot Anders <elliot@greenriver.org>
2026-07-01 13:48:43 -0400

    Make rubocop happy

--- db/migrate/20260701170632_add_vacancy_submission_config.rb
+++ db/migrate/20260701170632_add_vacancy_submission_config.rb
@@ -1,3 +1,11 @@
+###
+# Copyright Green River Data Group, Inc.
+#
+# License detail: https://github.com/greenriver/boston-cas/blob/stable/LICENSE.md
+###
+
+# frozen_string_literal: true
+
 class AddVacancySubmissionConfig < ActiveRecord::Migration[7.2]
   def change
     add_column :configs, :vacancy_submission_mechanism, :string, default: 'Traditional'
