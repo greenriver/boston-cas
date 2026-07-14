06690d7280bb4369655ece3bb38918b9c2e3e4c2
Elliot Anders <elliot@greenriver.org>
2026-07-14 13:50:04 -0400

    Missed frozen string literal

--- db/migrate/20260714153122_fix_default_casing.rb
+++ db/migrate/20260714153122_fix_default_casing.rb
@@ -1,3 +1,11 @@
+###
+# Copyright Green River Data Group, Inc.
+#
+# License detail: https://github.com/greenriver/boston-cas/blob/stable/LICENSE.md
+###
+
+# frozen_string_literal: true
+
 class FixDefaultCasing < ActiveRecord::Migration[7.2]
   def change
     change_column_default :configs, :vacancy_submission_mechanism, from: 'Traditional', to: 'traditional'
